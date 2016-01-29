b13932ffbe3320f41fc10ee54d4bbc6e9186a206
Anthony Borrow <anthony@moodle.org>
2016-01-29 15:56:32 -0600

    20160129 - adding auth to pages controller

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -10,7 +10,7 @@ class PagesController extends Controller
 {
      public function __construct()
     {
-        //$this->middleware('auth');
+        $this->middleware('auth');
     }
      /**
      * Display a listing of the resource.
