42a9f49e2c426c419b20edc68ba28d293547abe2
Anthony Borrow <anthony@moodle.org>
2016-03-08 11:11:52 -0600

    20160308 - address fillable fields

--- app/Address.php
+++ app/Address.php
@@ -9,6 +9,7 @@ class Address extends Model
 {
     use SoftDeletes;
     protected $table = 'address';
+    protected $fillable =  ['contact_id', 'location_type_id', 'is_primary', 'street_address', 'supplemental_address_1', 'city','state_province_id','postal_code','country_id'];
     
     public function owner() {
         return $this->belongsTo('\montserrat\Person','contact_id','id');
