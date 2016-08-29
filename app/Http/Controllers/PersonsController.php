b0f7cde088bf2080bc5baf485af5c2d2740263f0
Anthony Borrow <anthony@moodle.org>
2016-08-29 15:31:10 -0500

    20160829 - tweaking images, creating infrastructure for contact attachments and file management

--- app/Http/Controllers/PersonsController.php
+++ app/Http/Controllers/PersonsController.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Storage;
 use Image;
 use Illuminate\Support\Facades\File;
-
+use Intervention;
 
 class PersonsController extends Controller
 {
