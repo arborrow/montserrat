d5da335962d23dd987bb56ab97f623831e835f48
Anthony Borrow <anthony@moodle.org>
2016-09-11 13:00:27 -0500

    20160911 - send emails on errors

--- resources/views/errors/default.blade.php
+++ resources/views/errors/default.blade.php
@@ -1,9 +1,8 @@
 @extends('template')
 @section('content')
     <div class="content">
-        <div class="text-danger"> Oops! Looks like Fr. Anthony is using the SQL to corrupt Polanco.<br />
+        <div class="text-danger"> Oops! Looks like Fr. Anthony is using the SQL to corrupt Polanco.<br /></div>
         {!! Html::image('img/error.png', 'System Error',array('title'=>"General Error")) !!}
-        </div>
-        <p>{{$e}}</p>
+        <p>A report of this error has been submitted to Fr. Anthony who will hunt down the error and destroy it.</p>
     </div>
 @stop
