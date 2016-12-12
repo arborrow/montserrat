95a1389349faf89ab9efc8598a6ce7aad9ecb9b6
Anthony Borrow <anthony@moodle.org>
2016-12-11 21:25:32 -0600

    20161211 - if no existing avatar and deceased it shows a deceased avatar rather than default

--- app/Contact.php
+++ app/Contact.php
@@ -109,25 +109,28 @@ class Contact extends Model
     }
     
     public function getAvatarLargeLinkAttribute() {
-         if ($this->is_deceased) {
-            return "<img src='".url('img/dead.png')."' class='img-circle' style='height: 150px; padding:5px;'>";
-        }
-       
+         
         if (Storage::has('contact/'.$this->id.'/avatar.png')) {
             return "<img src='".url('avatar/'.$this->id)."' class='img-circle' style='height: 150px; padding:5px;'>";
         } else {
+            if ($this->is_deceased) {
+                return "<img src='".url('img/dead.png')."' class='img-circle' style='height: 150px; padding:5px;'>";
+            } else {
             return "<img src='".url('img/default.png')."' class='img-circle' style='height: 150px; padding:5px;'>";
+            }
         }
     
     }
     public function getAvatarSmallLinkAttribute() {
-        if ($this->is_deceased) {
-            return "<img src='".url('img/dead.png')."' class='img-circle' style='height: 75px; padding:5px;'>";
-        }
+        
         if (Storage::has('contact/'.$this->id.'/avatar.png')) {
             return "<img src='".url('avatar/'.$this->id)."' class='img-circle' style='height: 75px; padding:5px;'>";
         } else {
-            return "<img src='".url('img/default.png')."' class='img-circle' style='height: 75px; padding:5px;'>";
+            if ($this->is_deceased) {
+                return "<img src='".url('img/dead.png')."' class='img-circle' style='height: 75px; padding:5px;'>";
+            } else {
+                return "<img src='".url('img/default.png')."' class='img-circle' style='height: 75px; padding:5px;'>";
+            }
         }
     
     }
