5cf902ffa4ccced10d475ad004560e6bf27e8b40
Anthony Borrow <anthony@moodle.org>
2016-12-21 20:08:40 -0600

    20161221 - format emergency contact phone_alternate using mutator in emergency contact model

--- app/EmergencyContact.php
+++ app/EmergencyContact.php
@@ -33,5 +33,25 @@ class EmergencyContact extends Model
             $this->attributes['phone'] = $phone_formatted.','.$phone_extension;
         }
     }
+    public function setPhoneAlternateAttribute($phone) {
+        $phone_numeric = $phone;
+        $phone_numeric = str_replace(" ","",$phone_numeric);
+        $phone_numeric = str_replace("(","",$phone_numeric);
+        $phone_numeric = str_replace(")","",$phone_numeric);
+        $phone_numeric = str_replace("-","",$phone_numeric);
+        if (strpos($phone_numeric,',') > 0) {
+            $phone_extension = substr($phone_numeric,strpos($phone_numeric,',')+1);
+            $phone_numeric = substr($phone_numeric,0,strpos($phone_numeric,','));
+        }
+        if (strlen($phone_numeric) == 10) {
+            $phone_formatted = '('.substr($phone_numeric,0,3).') '.substr($phone_numeric,3,3).'-'.substr($phone_numeric,6,4);
+        }
+        if (empty($phone_extension)) {
+            $this->attributes['phone_alternate'] = $phone_formatted;
+        } else {
+            $this->attributes['phone_alternate'] = $phone_formatted.','.$phone_extension;
+        }
+    }
+
 
 }
