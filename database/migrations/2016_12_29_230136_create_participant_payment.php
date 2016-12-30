75340bbfc368f2a8aa4edadc3ccc4b2da6e49e09
Anthony Borrow <anthony@moodle.org>
2016-12-29 23:32:22 -0600

    20161229 - add contribution and participant_payment migrations

--- database/migrations/2016_12_29_230136_create_participant_payment.php
+++ database/migrations/2016_12_29_230136_create_participant_payment.php
@@ -14,13 +14,14 @@ class CreateParticipantPayment extends Migration
     {
         Schema::create('participant_payment', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('participant_id')->unsigned;
-            $table->integer('contribution_id')->unsigned;
+            $table->integer('participant_id')->unsigned();
+            $table->integer('contribution_id')->unsigned();
             $table->softDeletes();
             $table->rememberToken();
             $table->timestamps();
             $table->unique(['contribution_id','participant_id'],'UI_contribution_participant');
-            //$table->foreign('participant_id')->references('id')->on('participant');
+            $table->foreign('participant_id')->references('id')->on('participant');
+            $table->foreign('contribution_id')->references('id')->on('contribution');
         });
     }
 
