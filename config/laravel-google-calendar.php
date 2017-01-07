13443a69bd287e8f0836e7f5755b357499888089
Anthony Borrow <anthony@moodle.org>
2017-01-06 22:02:47 -0600

    20170106 - updating calendar integration json file for production

--- config/laravel-google-calendar.php
+++ config/laravel-google-calendar.php
@@ -5,7 +5,8 @@ return [
     /**
      * Path to a json file containing the credentials of a Google Service account.
      */
-    'client_secret_json' => storage_path('app/laravel-google-calendar/montserrat-dev-cb9265e52c2f.json'),
+    //'client_secret_json' => storage_path('app/laravel-google-calendar/montserrat-dev-cb9265e52c2f.json'),
+    'client_secret_json' => storage_path('app/laravel-google-calendar/montserrat-polanco-4debe428bb97.json'),
 
     /**
      *  The id of the Google Calendar that will be used by default.
