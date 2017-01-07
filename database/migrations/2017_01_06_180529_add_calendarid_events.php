f0e8574093d82514213300704f473da3611d0c9f
Anthony Borrow <anthony@moodle.org>
2017-01-06 18:15:44 -0600

    20170106 - migration to add calendar_id to events table

---
+++ database/migrations/2017_01_06_180529_add_calendarid_events.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCalendaridEvents extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::table('event', function ($table) {
+            $table->string('calendar_id')->nullable()->default(NULL);
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('event', function ($table) {
+            $table->dropColumn('calendar_id');
+        });
+    
+    }
+}
