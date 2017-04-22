f99ff1c61d96c6296b4c300465ba9841a6a5ff10
Laravel Shift <shift@laravelshift.com>
2017-04-22 18:33:44 +0000

    Shift cleanup

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace montserrat\Http\Controllers\Auth;
 
-use App\User;
+use montserrat\User;
 use Validator;
-use App\Http\Controllers\Controller;
+use montserrat\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\RegistersUsers;
 
 class RegisterController extends Controller
