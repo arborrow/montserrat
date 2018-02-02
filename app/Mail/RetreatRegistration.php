042b25c6767cb94407b2b0fc05d49d934ca0950d
Gulmaro Salinas <gul.salinasj@gmail.com>
2018-02-02 17:00:29 -0600

    Retreat Registration stub
    - creates stub to implement retreat registration emails

---
+++ app/Mail/RetreatRegistration.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace montserrat\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class RetreatRegistration extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->subject('Montserrat Jesuit Retreat House Registration')
+                    ->view('emails.retreat-registration');
+    }
+}
---
+++ resources/views/emails/retreat-registration.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<p>
+		Dear [first name],<br><br>
+
+		We are praying for you as your upcoming retreat approaches on [date]. You will find a beautiful campus, made holy by the thousands who have prayed here, that is both hospitable and restorative.<br><br>
+
+		Check-in for your retreat is between 4:00 – 6:00 p.m. on [day,date]. Dinner will be served from 6:00 – 7:15 p.m. When you arrive, please come to Campion Hall to receive your room key and retreat packet. Below are some possible suggestions regarding what to bring. Some frequently asked questions can be found on our FAQs webpage but feel free to call or email should you need more information.<br><br>
+
+		We look forward to welcoming and accompanying you during the retreat.<br><br>
+
+		In the Lord,<br><br>
+		Fr. Anthony Borrow, S.J.<br>
+		Director<br><br>
+
+		P.S. If you have a moment, it is helpful to confirm your attendance by clicking here [link]. If your plans have changed and you need to reschedule your retreat please call us at 940-321-6020 or send an email to registration@montserratretreat.org.<br><br>
+
+		What is provided:<br> 
+		• All meals, snacks and beverages<br>
+		• Bed linens, pillows, towels, and washcloths<br>
+		• Each room has a private bath and individually controlled cooling/heating units<br><br>
+
+		What to bring:<br>
+		• Personal medicines, toiletries and hairdryer<br>
+		• Comfortable, casual clothes and walking shoes<br>
+		• A pen, journal or notebook<br>
+		• A water bottle for taking beverages out of the dining hall<br>
+		• All buildings are air-conditioned in the summer, so a light jacket or sweater may be helpful<br>
+	</p>
+</body>
+</html>
