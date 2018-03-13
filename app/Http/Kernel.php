ec10b855c7ef83b349ac6f811168862e54985d99
Laravel Shift <shift@laravelshift.com>
2018-03-13 02:45:06 +0000

    Shift cleanup

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,7 +19,7 @@ class Kernel extends HttpKernel
         \montserrat\Http\Middleware\TrimStrings::class,
         \Illuminate\Session\Middleware\StartSession::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \App\Http\Middleware\TrustProxies::class,
+        \montserrat\Http\Middleware\TrustProxies::class,
     ];
 
     /**
