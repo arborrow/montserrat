f73bf59a51b354a0d2b49b69d3e7b5b958393c83
Gulmaro Salinas <gul.salinasj@gmail.com>
2018-03-24 00:48:41 -0500

    20180324 email birthdays command
    - creates command to email birthday

---
+++ app/Console/Commands/SendBirthdays.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Contact;
+use Illuminate\Console\Command;
+use App\Mail\RetreatantBirthday;
+
+class SendBirthdays extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'email:birthdays';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Sends out birthday emails to retreatants with birthdays of current day.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $receivers = new Contact;
+        $receivers = $receivers->birthdayEmailReceivers();//dd($receivers);
+        foreach ($receivers as $key => $receiver) {
+            try {
+                \Mail::to($receiver->email)->queue(new RetreatantBirthday($receiver));
+            } catch (\Exception $e) {
+                // don't do anything
+            }
+        }
+    }
+}
