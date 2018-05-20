17f830227c28994a4f92d9ddac897cf321c0f9bb
Anthony Borrow <anthony@moodle.org>
2018-05-19 23:14:14 -0500

    #171 - minor changes to confirmation email text

--- resources/views/emails/retreat-confirmation.blade.php
+++ resources/views/emails/retreat-confirmation.blade.php
@@ -7,9 +7,9 @@
 	<p>
 		Dear {{ $participant->retreatant->first_name }}, <br><br>
 
-		We are praying for you as your upcoming retreat approaches on {{ $participant->retreat_start_date->format('F n, Y') }}. You will find a beautiful campus, made holy by the thousands who have prayed here, that is both hospitable and restorative.<br><br>
+		We are praying for you as your upcoming retreat approaches. You will find a beautiful campus, made holy by the thousands who have prayed here, that is both hospitable and restorative.<br><br>
 
-		Check-in for your retreat is between 4:00 – 6:00 p.m. on {{ $participant->retreat_start_date->format('l F j, Y') }}. Dinner will be served from 6:00 – 7:15 p.m. When you arrive, please come to Campion Hall to receive your room key and retreat packet. Below are some possible suggestions regarding what to bring. Some frequently asked questions can be found on our FAQs webpage but feel free to call or email should you need more information.<br><br>
+		Check-in for your retreat is between 4:00 – 6:00 p.m. on {{ $participant->retreat_start_date->format('l F j, Y') }}. Dinner will be served from 6:00 – 7:00 p.m. When you arrive, please come to Campion Hall to receive your room key and retreat packet. Below are some possible suggestions regarding what to bring. Some frequently asked questions can be found on our FAQs webpage but feel free to call or email should you need more information.<br><br>
 
 		We look forward to welcoming and accompanying you during the retreat.<br><br>
 
@@ -17,7 +17,7 @@
 		Fr. Anthony Borrow, S.J.<br>
 		Director<br><br>
 
-		P.S. If you have a moment, it is helpful to confirm your attendance by <a href="{{ url('intercept/'.$encodedUrl ) }}">clicking here</a>. If your plans have changed and you need to reschedule your retreat please call us at 940-321-6020 or send an email to registration@montserratretreat.org.<br><br>
+		P.S. If you have a moment, it is helpful if you could <a href="{{ url('intercept/'.$encodedUrl ) }}">click here to confirm your attendance</a>. If your plans have changed kindly let us know by calling us at 940-321-6020 or emailing us at registration@montserratretreat.org. We would be happy to help you reschedule your retreat.<br><br>
 
 		What is provided:<br> 
 		• All meals, snacks and beverages<br>
