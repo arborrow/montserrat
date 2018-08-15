39521bddfb9570ca770108756e9d1b7eb8949ab7
Anthony Borrow <arborrow@jesuits.net>
2018-08-15 08:22:45 -0500

    #211 - adding Spanish retreat confirmation email blade

---
+++ resources/views/emails/retreat-confirmation-es.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title></title>
+</head>
+<body>
+<p>
+		Saludos: {{ $participant->retreatant->first_name }}, <br><br>
+
+		Estamos rezando por usted durante este tiempo de preparación para su retiro que se acerca pronto. Encontrará vistas hermosas y tierra santa sanctificado por las oraciones de la gran cantidad de personas que han rezado aqui en Montserrat. Esperamos la oportunidad de hospedarle para esta experiencia restaurativa e iluminante.<br><br>
+
+		Le esperamos entre las 4:00 - 6:00 de la tarde el {{ $participant->retreat_start_date_es }} para registrarse la entrada. Empezamos de servir la cena a las 6:00 de la tarde hasta las 7:00. Al llegar, favor de reportarse a la Sala de Campion para recibir la llave para su recamara y un folleto de materiales para el retiro. Abajo notamos unas surgerencias de lo que ofrecemos, lo que va a necesitar para el retiro y lo que no va a necesitar. También hay una página de web de las preguntas mas frecuentes. Si tiene alguna duda favor de llamarnos en la oficina (940-321-6020) o mandar un correo-electrónico para mas información.<br><br>
+
+		Si tiene un momentito, favor de <a href="{{ url('intercept/'.$encodedUrl ) }}">hacer clic para confirmar que va a asistir el retiro</a>. Si cambian sus planes, por favor avísanos lo antes posible por teléfono (940-321-6020) o por correo-electrónico (registrar@montserratretreat.org), ya que hay una lista de espera para el retiro y queremos acomodar a todos los que quieren venir. Podemos ayudarle cambiar el retiro para otra fecha mas conveniente.  <br><br>
+
+		Una vez más, estamos encantados y agradecidos de que usted haga que la tierra de Montserrat sea sagrada con sus oraciones aquí. Si usted tiene alguna pregunta antes de la llegada, contáctenos por correo electrónico o llame a la oficina. Esperamos darle la bienvenida pronto. Hasta entonces, oremos el uno por el otro.<br><br>
+
+Paz - Padre Antonio Borrow, S.J., Director<br>
+director@montserratretreat.org <br>
+940-321-6020<br><br>
+
+Lo que debe traer: <br>
+• Medicamentos personales, artículos de tocador y secadora de pelo <br>
+• Ropa cómoda y casual y zapatos para caminar <br>
+• Un cuaderno o páginas para escribir sus propias oraciones, reflexiones, notas o experiencias <br>
+• Cada habitación tiene una Biblia, pero usted puede traer la suya también<br><br>
+
+Lo que ofrecemos:<br>
+• Todas las comidas, bocadillos y refrescos<br>
+• Toallas, sábanas, y almohadas<br>
+• Cada recamara tiene baño privado y aire condicionado <br><br>
+
+Para dejar en casa: <br>
+• Computadoras, tabletas, etc. - Poder desconectar es importante para su experiencia de retiro.<br>
+• Teléfonos: Le pedimos que apague y guarde todos los teléfonos una vez que llegue a Montserrat. Por favor, proporcione este número de teléfono a quienes lo necesiten para fines de contacto de emergencia: 940-220-9168. El personal de Montserrat le notificará si recibe un mensaje.<br><br>
+
+</p>
+</body>
+</html>
