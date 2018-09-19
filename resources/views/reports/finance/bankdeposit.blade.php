9bd69341d7c32f73ccde16f12c436eb0dbf8ca24
Anthony Borrow <anthony@moodle.org>
2018-09-19 16:13:56 -0500

    20180919 - adding bank deposit report

--- resources/views/reports/finance/bankdeposit.blade.php
+++ resources/views/reports/finance/bankdeposit.blade.php
@@ -1,41 +1,40 @@
-@extends('reportlandscape')
+@extends('report')
 @section('content')
 
-<div class ="retreatlisting">
-@if (!$registrations->isEmpty())
-<h2>Retreat Listing for Retreat #{{$registrations[0]->retreat->idnumber}} - {{$registrations[0]->retreat->title}}</h2> 
+<div class ="bankdeposit">
+@if (!$grouped_payments->isEmpty())
+<h2>Bank Deposit Report for {{$report_date}}</h2> 
      
 <hr />
+@foreach($grouped_payments as $donation_description => $payments)
+<strong>{{$donation_description}} </strong><br />
+    
  <table width="100%">
-        <th class="row-1 row-name">Full name</th>
-        <th class="row-2 row-address">Address</th>
-        <th class="row-3 row-city">City, State, Zip</th>
-        <th class="row-4 row-phone">Cell phone</th>
-        <th class="row-5 row-phone">Home phone</th>
-        <th class="row-6 row-phone">Parish</th>
-        <th class="row-7 row-notes">Notes</th>
+        <th class="row-2 row-donor">Donor</th>
+        <th class="row-3 row-retreat_id">Retreat ID</th>
+        <th class="row-4 row-retreat_date">Retreat Date</th>
+        <th class="row-5 row-pledged_amount">Pledged amount</th>
+        <th class="row-6 row-payment_amount">Payment amount</th>
+        <th class="row-7 row-payment_description">Payment description</th>
     
                 
-    @foreach($registrations as $registration)
-    
+    @foreach($payments as $payment)
     <tr>
-        <td>{{$registration->retreatant->display_name}}</td>
-        <td>{{$registration->retreatant->address_primary_street}}</td>
-        <td>{{$registration->retreatant->address_primary_city}}, {{$registration->retreatant->address_primary_state}}  {{$registration->retreatant->address_primary_postal_code}}</td>
-        <td>{{$registration->retreatant->phone_home_mobile_number}}</td>
-        <td>{{$registration->retreatant->phone_home_phone_number}}</td>
-        <td>{{$registration->retreatant->parish_name}}</td> 
-        <td>{{$registration->retreatant->note_regsitration_text}}</td>
-        
+        <td>{{$payment->donation->contact->display_name}}</td>
+        <td>{{$payment->donation->retreat->idnumber}}</td>
+        <td>{{$payment->donation->retreat->start_date}}</td>
+        <td>${{number_format($payment->donation->donation_amount,2)}}</td>
+        <td>${{number_format($payment->payment_amount,2)}}</td> 
+        <td>{{$payment->payment_description}}</td>
     </tr>    
-    @endforeach
+    @endforeach  
+ </table> 
+<strong>Total of {{$payments->count()}} payments for {{$donation_description}} totaling ${{number_format($payments->sum('payment_amount'),2)}} </strong>
+    <hr />
+   @endforeach
    @endIf    
-</table>
 <br />
 
-<hr />
-<strong>{{$registrations->count()}} Registered Retreatant(s) as of {{date('l, F j, Y')}}</strong>
-<hr />
         <span class="logo">
             {!! Html::image('img/mrhlogoblack.png','Home',array('title'=>'Home','class'=>'logo','align'=>'right')) !!}
        
@@ -45,6 +44,7 @@
                 Lake Dallas, TX 75065<br />
                 (940) 321-6020<br /> 
             <a href='http://montserratretreat.org/' target='_blank'>montserratretreat.org</a>
+            Report run: as of {{date('l, F j, Y')}}
         
     </span>
 </div>
