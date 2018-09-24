d71bb01c5cbc7cbce54d6f1da1db6265444e4235
Anthony Borrow <arborrow@jesuits.net>
2018-09-24 11:48:27 -0500

    #214  - updating database factory and seeds; removing diocese table creation migration as this table is not needed

---
+++ database/factories/LocationFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Location::class, function (Faker $faker) {
+    return [
+        'name' => $faker->lastname.' Hall',
+        'description' => $faker->sentence,
+        'occupancy' => $faker->numberBetween(10, 50), 
+        'notes' => $faker->text(100),
+    ];
+});
+
