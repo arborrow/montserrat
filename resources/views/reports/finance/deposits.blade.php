c23da4b1ae68089f2d26abc4dd0b05b7a5c60d7a
Anthony Borrow <arborrow@jesuits.net>
2018-09-25 14:18:17 -0500

    20180925 - tweaking finance deposit report to use payments instead of donations

--- resources/views/reports/finance/deposits.blade.php
+++ resources/views/reports/finance/deposits.blade.php
@@ -2,32 +2,30 @@
 @section('content')
 
 <div class ="retreat_deposits">
-<h2>Open Deposits Report </h2> 
-@if (!$grouped_donations->isEmpty())
+<h2>Open Deposit Report </h2> 
+@if (!$grouped_payments->isEmpty())
      
 <hr />
-@foreach($grouped_donations as $retreat_id => $group_donations)
+@foreach($grouped_payments as $retreat_id => $group_payments)
 <strong>{{$retreat_id}} </strong><br />
     
  <table width="100%">
         <th class="row-donor">Donor</th>
         <th class="row-pledged">Date</th>
         <th class="row-pledged">Amount</th>
-        <th class="row-paid">Retreat</th>
                 
-    @foreach($group_donations->sortBy('contact.sort_name') as $donation)
+    @foreach($group_payments->sortBy('donation.contact.sort_name') as $payment)
     <tr>
-        <td>{{$donation->contact->display_name}}</td>
-        <td>{{$donation->payments->max('payment_date')}}</td>
-        <td>${{number_format($donation->payments->sum('payment_amount'),2)}}</td>
-        <td>{{$donation->retreat->title.'('.$donation->retreat->idnumber.')'.' ['.$donation->retreat->start_date.']' }}</td>
+        <td>{{$payment->donation->contact->display_name}}</td>
+        <td>{{$payment->payment_date->format('m/d/Y')}}</td>
+        <td>${{number_format($payment->payment_amount,2)}}</td>
     </tr>    
     @endforeach  
  </table> 
-<strong>Total of {{$group_donations->count()}} Deposits for {{$donation->retreat->title}} totaling ${{number_format($group_donations->sum('payments_paid'),2)}} </strong>
+<strong>Total of {{$group_payments->count()}} Deposit payments for {{$retreat_id}} totaling ${{number_format($group_payments->sum('payment_amount'),2)}} </strong>
     <hr />
    @endforeach
-   <strong>Grand total of all payments: ${{number_format($donations->sum('payments_paid'),2)}} <br />
+   <strong>Grand total of all payments: ${{number_format($payments->sum('payment_amount'),2)}} <br />
    @endIf    
 <br />
 
