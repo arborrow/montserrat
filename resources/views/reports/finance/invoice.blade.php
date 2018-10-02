ddaeb2e190c759137f4e79fba9006f800262292e
Anthony Borrow <anthony@moodle.org>
2018-10-02 14:08:09 -0500

    20181002 - tweaking invoices

--- resources/views/reports/finance/invoice.blade.php
+++ resources/views/reports/finance/invoice.blade.php
@@ -2,23 +2,36 @@
 @section('content')
 
 <div class ="invoice">
-<img src="https://polanco.montserratretreat.org/img/mrhlogoblack.png"> <br />
-600 N. Shady Shores Dr <br />
-Lake Dallas, TX 75065 <br />
-(940) 321-6020 <br />
-(940) 321-6040 - Fax <br />
-montserratretreat.org <br /> <br /> 
-<div style="text-align: center">><h2>Invoice# {{$donation->donation_id}} for {{$donation->retreat->title}}</h2></div><br /> 
+    <div class ="pageheader">
+        
+<table>
+    <tr>
+        <td>
+            <img src="https://polanco.montserratretreat.org/img/mrhlogoblack.png"> <br />
+            600 N. Shady Shores Dr <br />
+            Lake Dallas, TX 75065 <br />
+            (940) 321-6020 <br />
+            (940) 321-6040 - Fax <br />
+            montserratretreat.org <br /> <br />
+        </td>
+        <td>
+            <h3>Invoice# {{$donation->donation_id}} for {{$donation->retreat->title}}</h3>
+        </td>
+    </tr>
+</table>
+        </div>
+<br /> 
 <br />
 {{$donation->contact->display_name}}<br />
 {{$donation->Notes1}} <br />
 {{$donation->contact->address_primary_street}} <br />     
 {{$donation->contact->address_primary_city}}, {{$donation->contact->address_primary_state}}  {{$donation->contact->address_primary_postal_code}} <br /><br />    
-<h2>Payments</h2>   
- <table width="100%" style="border: 1px solid black">
+<div class="payments">
+    <h3>Payments</h3>   
+ <table>
         <th class="row-payment_date">Date</th>
-        <th class="row-payment_type">Payment type</th>
-        <th class="row-payment_number">#</th>
+        <th class="row-payment_type">Payment method</th>
+        <th class="row-payment_number">Reference #</th>
         <th class="row-payment_amount">Amount</th>
         <th class="row-payment_note">Notes</th>
                 
@@ -27,24 +40,38 @@ montserratretreat.org <br /> <br />
         <td>{{$payment->payment_date->format('m/d/Y')}}</td>
         <td>{{$payment->payment_description}}</td>
         <td>{{$payment->payment_number}}</td>
-        <td>${{number_format($payment->payment_amount,2)}}</td>
+        <td class="row-payment_amount">${{number_format($payment->payment_amount,2)}}</td>
         <td>{{$payment->note}}</td>
     </tr>    
     @endforeach  
- </table> 
+ </table>
+</div>
 <br />
-<h2>
-Total amount: ${{number_format($donation->donation_amount,2)}} <br />
-Total payments: ${{number_format($donation->payments_paid,2)}} <br />
-Current amount due: ${{number_format(($donation->donation_amount - $donation->payments_paid),2)}} <br />
-</h2>   
+<div class="invoice-totals">
+<table>
+    <tr>
+        <td>Total amount: </td>
+        <td class="row-payment_amount"> ${{number_format($donation->donation_amount,2)}} </td>
+    </tr>
+    <tr>
+        <td>Total payments: </td>
+        <td class="row-payment_amount">${{number_format($donation->payments_paid,2)}} </td>
+    </tr>
+    <tr>
+        <td>Current amount due:</td>
+        <td class="row-payment_amount">${{number_format(($donation->donation_amount - $donation->payments_paid),2)}} </td>
+    </tr>    
+ 
+</table>
+</div>
 <br />
-Description: {{$donation->donation_description}} <br />
-Notes: {{$donation->Notes}} <br />
-Terms: {{$donation->terms}} <br /> 
+<strong>Description:</strong> {{$donation->donation_description}} <br /><br />
+<strong>Notes:</strong> {{$donation->Notes}} <br /><br />
+<strong>Terms:</strong> {{$donation->terms}} <br /> <br />
 <br />
+<i>Make payments payable to: <strong>Montserrat Jesuit Retreat House</strong></i> <br /><br />
+
 <span class='pagefooter'>
-Make payments payable to: Montserrat Jesuit Retreat House <br /><br />
     Invoice date: {{date('l, F j, Y H:i')}}
 </span>
 </div>
