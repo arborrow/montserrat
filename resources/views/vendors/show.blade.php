137a4cee97ed818e4698aff6d05c7968042742f3
Anthony Borrow <arborrow@jesuits.net>
2018-10-06 19:22:54 -0500

    20181006 - adding anchors to various show blades; adding donations section to various show blades

--- resources/views/vendors/show.blade.php
+++ resources/views/vendors/show.blade.php
@@ -5,25 +5,43 @@
     <div class="jumbotron text-left">
         <div class="panel panel-default">
             <div class="panel-heading">
-                <div class="col-md-12 col-sm-12">
-                    {!!$vendor->avatar_large_link!!}
-                    <h1 style="position: absolute; top:5px; left:175px; padding: 5px;">
-                        <strong>
-                            @can('update-contact')
-                                <a href="{{url('vendor/'.$vendor->id.'/edit')}}">{{ $vendor->organization_name }}</a>
-                            @else
-                                {{ $vendor->organization_name }}
-                            @endCan
-                        </strong>
-                    </h1>
+                <div class='row' style="height: 200px;">
+            
+                    <div class="col-md-12 col-sm-12">
+                        {!!$vendor->avatar_large_link!!}
+                        <h1 style="position: absolute; top:5px; left:175px; padding: 5px;">
+                            <strong>
+                                @can('update-contact')
+                                    <a href="{{url('vendor/'.$vendor->id.'/edit')}}">{{ $vendor->organization_name }}</a>
+                                @else
+                                    {{ $vendor->organization_name }}
+                                @endCan
+                            </strong>
+                        </h1>
+                    </div>
+                    {!! Html::link('#notes','Notes',array('class' => 'btn btn-default')) !!}
+                    {!! Html::link('#relationships','Relationships',array('class' => 'btn btn-default')) !!}
+                    {!! Html::link('#registrations','Registrations',array('class' => 'btn btn-default')) !!}
+                    {!! Html::link('#touchpoints','Touchpoints',array('class' => 'btn btn-default')) !!}
+                    {!! Html::link('#attachments','Attachments',array('class' => 'btn btn-default')) !!}
+                    {!! Html::link('#donations','Donations',array('class' => 'btn btn-default')) !!}
                 </div>
-                <span class="back"><a href={{ action('VendorController@index') }}>{!! Html::image('img/vendor.png', 'Vendor Index',array('title'=>"Vendor Index",'class' => 'btn btn-primary')) !!}</a></span></h1>
-                @can('create-touchpoint')
-                    <span class="btn btn-primary">
-                        <a href={{ action('TouchpointController@add',$vendor->id) }}>Add Touchpoint</a>
+                <div class="clearfix"> </div>
+                
+                <div class='row'>
+                    
+                    <span class="back">
+                        <a href={{ action('VendorController@index') }}>{!! Html::image('img/vendor.png', 'Vendor Index',array('title'=>"Vendor Index",'class' => 'btn btn-primary')) !!}</a>
                     </span>
-                @endCan
+                    @can('create-touchpoint')
+                        <span class="btn btn-primary">
+                            <a href={{ action('TouchpointController@add',$vendor->id) }}>Add Touchpoint</a>
+                        </span>
+                    @endCan
+                </div>
+                <div class="clearfix"> </div>
             </div>
+            
             <div class='row'>
                 <div class='col-md-4'>
                     <span>
@@ -44,6 +62,7 @@
                 </div>
             </div>
             <div class="clearfix"> </div>
+
             <div class='row'>
                 <div class='col-md-4'>
                     <span>
@@ -55,10 +74,10 @@
                         @endforeach
                     </span>
                 </div>
-                
+
             </div>
             <div class="clearfix"> </div>
-                
+
             <div class='row'>
                 <div class='col-md-4'>
                     <span>
@@ -77,21 +96,25 @@
                 </div>
             </div>
             <div class="clearfix"> </div>
-            <div class='row'>
-                <div class='col-md-6'>
-                    <span>
-                        <h2>Notes</h2>
-                        @foreach($vendor->notes as $note)
-                            @if(!empty($note->note))
-                                <strong>{{$note->subject}}: </strong>{{$note->note}} (modified: {{$note->modified_date}})<br />
-                            @endif
-                        @endforeach
-                    </span>
+
+            <div class='row' id="notes">
+                <div class='col-md-12'>
+                    <div class='panel-heading'>
+                        <span>
+                            <h2><strong>Notes</strong></h2>
+                        </span>
+                    </div>
+                    @foreach($vendor->notes as $note)
+                        @if(!empty($note->note))
+                            <strong>{{$note->subject}}: </strong>{{$note->note}} (modified: {{$note->modified_date}})<br />
+                        @endif
+                    @endforeach
                 </div>
             </div>
             <div class="clearfix"> </div>
-            <div class='row'>
-                <div class='col-md-8'>
+
+            <div class='row' id="relationships">
+                <div class='col-md-12'>
                     <div class='panel-heading'>
                         <h2><strong>Relationships</strong></h2>
                     </div>
@@ -134,8 +157,8 @@
             </div>
             <div class="clearfix"> </div>
 
-            <div class='row'>
-                <div class='col-md-8'>
+            <div class='row' id="registrations">
+                <div class='col-md-12'>
                     <div class='panel-heading'>
                         <h2><strong>Retreat Participation for {{ $vendor->display_name }}</strong></h2>
                     </div>
@@ -147,9 +170,54 @@
                 </div>
             </div>
             <div class="clearfix"> </div>
-        
-            <div class='row'>
-                <div class='col-md-8'>
+
+            <div class='row' id="touchpoints">
+            @can('show-touchpoint')  
+                <div class='col-md-12'>
+                    <div class="panel-heading">
+                        <h2>
+                            <span class="grey">
+                                <strong>Touchpoints for {{ $vendor->display_name }}</strong> 
+                            </span>
+                        </h2>
+                        @can('create-touchpoint')
+                            <span class="btn btn-primary">
+                                <a href={{ action('TouchpointController@add',$vendor->id) }}>Add Touchpoint</a>
+                            </span>
+                        @endCan
+                    </div>
+
+                @if ($vendor->touchpoints->isEmpty())
+                    <p>It is a brand new world, there are no touchpoints for this person!</p>
+                @else
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>Date</th>
+                                <th>Contacted by</th>
+                                <th>Type of contact</th>
+                                <th>Notes</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($vendor->touchpoints as $touchpoint)
+                            <tr>
+                                <td><a href="../touchpoint/{{ $touchpoint->id}}">{{ $touchpoint->touched_at }}</a></td>
+                                <td><a href="../person/{{ $touchpoint->staff->id}}">{{ $touchpoint->staff->display_name }}</a></td>
+                                <td>{{ $touchpoint->type }}</td>
+                                <td>{{ $touchpoint->notes }}</td>
+                            </tr>
+                            @endforeach
+
+                        </tbody>
+                    </table>
+                @endif
+                </div>
+            @endCan
+            </div>
+
+            <div class='row' id="attachments">
+                <div class='col-md-12'>
                     <div class='panel-heading'>
                         <h2><strong>Attachments for {{ $vendor->display_name }} </strong></h2>
                     </div>
@@ -179,62 +247,63 @@
                     @endif
                 </div>
             </div>
-            @can('show-touchpoint')   
-                <div class="panel panel-default">
-                    @if ($vendor->touchpoints->isEmpty())
-                        <div class="panel-heading">
-                            <h2>
-                                <span class="grey">Touchpoints for {{ $vendor->display_name }} </span>
-                            </h2> 
-                        </div>
-                        <p>It is a brand new world, there are no touchpoints for this person!</p>
+            <div class="clearfix"> </div>
+
+            <div class='row' id='donations'>
+            @can('show-donation')
+                <div class='col-md-8'>
+                    <div class='panel-heading'>
+                        <h2><strong>Donations for {{ $vendor->display_name }} ({{$vendor->donations->count() }} donations totaling:  ${{ number_format($vendor->donations->sum('donation_amount'),2)}})</strong></h2>
+                        @can('create-donation')
+                            {!! Html::link(action('DonationController@create',$vendor->id),'Create donation',array('class' => 'btn btn-default'))!!}
+                        @endCan
+                    </div>
+
+                    @if ($vendor->donations->isEmpty())
+                            <p>No donations for this vendor!</p>
                     @else
-                        <div class="panel-heading">
-                            <h2>
-                                <span class="grey">Touchpoints for {{ $vendor->display_name }} </span>
-                            </h2> 
-                            <span class="btn btn-primary">
-                               <a href={{ action('TouchpointController@add',$vendor->id) }}>Add Touchpoint</a>
-                            </span>
-                        </div>
-                        <table class="table">
+                        <table class="table table-striped">
                             <thead>
                                 <tr>
                                     <th>Date</th>
-                                    <th>Contacted by</th>
-                                    <th>Type of contact</th>
+                                    <th>Description</th>
+                                    <th>Paid / Pledged</th>
+                                    <th>Terms</th>
                                     <th>Notes</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                @foreach($vendor->touchpoints as $touchpoint)
-                                <tr>
-                                    <td><a href="../touchpoint/{{ $touchpoint->id}}">{{ $touchpoint->touched_at }}</a></td>
-                                    <td><a href="../person/{{ $touchpoint->staff->id}}">{{ $touchpoint->staff->display_name }}</a></td>
-                                    <td>{{ $touchpoint->type }}</td>
-                                    <td>{{ $touchpoint->notes }}</td>
-                                </tr>
-                                @endforeach
 
+                            @foreach($vendor->donations as $donation)
+                                <tr>
+                                    <td><a href="../donation/{{$donation->donation_id}}"> {{ $donation->donation_date }} </a></td>
+                                    <td> {{ $donation->donation_description }}</td>
+                                    <td> ${{number_format($donation->payments->sum('payment_amount'),2)}} / ${{ number_format($donation->donation_amount,2) }} </td>
+                                    <td> {{ $donation->terms }}</td>
+                                    <td> {{ $donation->Notes }}</td>
+                                </tr>
+                            @endforeach
                             </tbody>
                         </table>
                     @endif
                 </div>
             @endCan
-        </div>
-        <div class='row'>
-            @can('update-contact')
-            <div class='col-md-1'>
-                <a href="{{ action('VendorController@edit', $vendor->id) }}" class="btn btn-info">{!! Html::image('img/edit.png', 'Edit',array('title'=>"Edit")) !!}</a>
             </div>
-            @endCan
-            @can('delete-contact')
-            <div class='col-md-1'>
-                {!! Form::open(['method' => 'DELETE', 'route' => ['vendor.destroy', $vendor->id],'onsubmit'=>'return ConfirmDelete()']) !!}
-                {!! Form::image('img/delete.png','btnDelete',['class' => 'btn btn-danger','title'=>'Delete']) !!} 
-                {!! Form::close() !!}
+            
+            <div class='row'>
+                @can('update-contact')
+                    <div class='col-md-1'>
+                        <a href="{{ action('VendorController@edit', $vendor->id) }}" class="btn btn-info">{!! Html::image('img/edit.png', 'Edit',array('title'=>"Edit")) !!}</a>
+                    </div>
+                @endCan
+                @can('delete-contact')
+                    <div class='col-md-1'>
+                        {!! Form::open(['method' => 'DELETE', 'route' => ['vendor.destroy', $vendor->id],'onsubmit'=>'return ConfirmDelete()']) !!}
+                        {!! Form::image('img/delete.png','btnDelete',['class' => 'btn btn-danger','title'=>'Delete']) !!} 
+                        {!! Form::close() !!}
+                    </div>
+                @endCan
             </div>
-            @endCan
             <div class="clearfix"> </div>
         </div>
     </div>
