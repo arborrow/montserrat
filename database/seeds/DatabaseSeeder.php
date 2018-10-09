13a01568ab8a500779d33dc76a23d8b2ee11d559
Ata Gowani <atagowani@gmail.com>
2018-10-09 11:50:23 -0500

    Make role to user seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -40,5 +40,6 @@ class DatabaseSeeder extends Seeder
         $this->call(TouchcategoriesTableSeeder::class);
         $this->call(DonationTypeTableSeeder::class);
         $this->call(PermissionRoleTableSeeder::class);
+        $this->call(RoleUserTableSeeder::class);
     }
 }
