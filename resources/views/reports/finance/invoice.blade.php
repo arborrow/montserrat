267e0932ecb810a692a263408a8dc99252922f80
Anthony Borrow <arborrow@jesuits.net>
2018-10-10 09:09:19 -0500

    20181010 - tweaks to invoices

--- resources/views/reports/finance/invoice.blade.php
+++ resources/views/reports/finance/invoice.blade.php
@@ -12,7 +12,7 @@
             Lake Dallas, TX 75065 <br />
             (940) 321-6020 <br />
             (940) 321-6040 - Fax <br />
-            montserratretreat.org <br /> <br />
+            montserratretreat.org <br /> 
         </td>
         <td>
             <h3 style="text-align: right">Date: {{date('m/d/Y')}}<br />
@@ -23,17 +23,27 @@
         </div>
 <br /> 
 {{$donation->contact->display_name}}<br />
+
 @if (isset($donation->Notes1))
     {{$donation->Notes1}} <br />
 @endIf
+
 {{$donation->contact->address_primary_street}} <br />     
 {{$donation->contact->address_primary_city}}, {{$donation->contact->address_primary_state}}  {{$donation->contact->address_primary_postal_code}} 
 <br /><br />
+
+@if(isset($donation->retreat_name))
 <strong>Event:</strong> {{$donation->retreat_name}} ({{$donation->retreat_idnumber}})
+@endIf
+
 <div class="payments">
 <br />    
-<strong>Notes:</strong> {{$donation->Notes}} <br /><br />
 
+@if(isset($donation->Notes))
+<strong>Notes:</strong> {{$donation->Notes}} <br /><br />
+@endIf
+
+@if($donation->payments->count()>0)
  <table>
      <caption style="text-align:left"><strong>Payments</strong></caption>
         <th class="row-payment_date">Date</th>
@@ -50,13 +60,15 @@
     </tr>    
     @endforeach  
  </table>
+@endIf
+
 </div>
 <br />
 <div class="invoice-totals">
 <table>
     <tr>
-        <td>Total amount: </td>
-        <td class="row-payment_amount"> ${{number_format($donation->donation_amount,2)}} </td>
+        <td style="width: 57%">Total amount: </td>
+        <td class="row-payment_amount" style="width: 43%"> ${{number_format($donation->donation_amount,2)}} </td>
     </tr>
     <tr>
         <td>Total payments: </td>
@@ -66,16 +78,22 @@
         <td><strong>Balance due:</strong></td>
         <td class="row-payment_amount"><strong>${{number_format(($donation->donation_amount - $donation->payments_paid),2)}}</strong> </td>
     </tr>    
- @if(isset($donation->donation_install))
+ 
+@if(isset($donation->donation_install))
  <tr>
      <td><strong>Current amount due:</strong></td>
      <td class="row-payment_amount"><strong>${{number_format(($donation->donation_install),2)}}</strong></td>
  </tr>
  @endif
+
 </table>
 </div>
 <br />
+
+@if(isset($donation->terms))
 <strong>Terms:</strong> {{$donation->terms}} <br /> <br />
+@endIf
+
 <br />
 <i>Please remit payment to: <br />
     <strong>Montserrat Jesuit Retreat House</strong></i> <br />
