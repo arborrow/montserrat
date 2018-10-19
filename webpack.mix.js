a5c36c9b112e72bb4bf611f69414986395bf05af
Ata Gowani <atagowani@gmail.com>
2018-10-18 22:15:00 -0500

    Add Bootstrap 4

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,4 +13,7 @@ const { mix } = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
+
+mix.styles(['public/css/app.css',
+            'public/css/all.css'], 'public/css/bundle.css');
    
