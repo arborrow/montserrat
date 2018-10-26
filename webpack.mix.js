3b22b9161a3ba2d2fe515f0d05ba0545ae911a61
Ata Gowani <atagowani@gmail.com>
2018-10-25 20:06:04 -0500

    Fix the bug generated due to asynchronicity

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,14 +12,19 @@ const glob = require('@alexbinary/glob');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/dist/js')
-   .sass('resources/assets/sass/app.scss', 'public/dist/css');
+var transpile = new Promise((resolve) => {
+  mix.js('resources/assets/js/app.js', 'public/dist/js')
+      .sass('resources/assets/sass/app.scss', 'public/dist/css');
 
-glob('public/dist/css/*.css').then((files) => {
-  mix.styles(files, 'public/dist/bundle.css').version();
-});
+  resolve();
+})
 
-glob('public/dist/js/*.js').then((files) => {
-  mix.styles(files, 'public/dist/bundle.js').version();
-});
-   
+transpile.then(() => {
+  glob('public/dist/css/*.css').then((files) => {
+    mix.styles(files, 'public/dist/bundle.css').version();
+  });
+
+  glob('public/dist/js/*.js').then((files) => {
+    mix.styles(files, 'public/dist/bundle.js').version();
+  });
+})
