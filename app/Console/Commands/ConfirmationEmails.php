3585657b8d4fc8c4556e46bed4acd4c29cc3d809
Anthony Borrow <arborrow@jesuits.net>
2018-11-27 14:18:49 -0600

    20181127 - merging in email fixes

--- app/Console/Commands/ConfirmationEmails.php
+++ app/Console/Commands/ConfirmationEmails.php
@@ -11,90 +11,90 @@ use Illuminate\Mail\Mailer;
 
 class ConfirmationEmails extends Command
 {
-	/**
-	 * The name and signature of the console command.
-	 *
-	 * @var string
-	 */
-	protected $signature = 'email:confirmations';
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'email:confirmations';
 
-	/**
-	 * The console command description.
-	 *
-	 * @var string
-	 */
-	protected $description = 'Send out confirmation emails 1 week in advance for a retreat';
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Send out confirmation emails 1 week in advance for a retreat';
 
-	protected $mailer;
+    protected $mailer;
 
-	/**
-	 * Create a new command instance.
-	 *
-	 * @return void
-	 */
-	public function __construct(Mailer $mailer)
-	{
-		parent::__construct();
-		$this->mailer = $mailer;
-	}
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct(Mailer $mailer)
+    {
+        parent::__construct();
+        $this->mailer = $mailer;
+    }
 
-	/**
-	 * Execute the console command.
-	 *
-	 * @return mixed
-	 */
-	public function handle()
-	{
-		$startDate = Carbon::today()->addDays(7);
-		$retreats = \App\Retreat::whereDate('start_date', $startDate)
-			->where('event_type_id', config('polanco.event_type.ignatian'))
-			->get();
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $startDate = Carbon::today()->addDays(7);
+        $retreats = \App\Retreat::whereDate('start_date', $startDate)
+            ->where('event_type_id', config('polanco.event_type.ignatian'))
+            ->get();
 
-		if ($retreats->count() >= 1) {
-			$automaticSuccessMessage = "Automatic confirmation email has been sent for retreat #".$retreat->idnumber.".";
-			$automaticErrorMessage = "Automatic confirmation email failed to send for retreat #".$retreat->idnumber.".";
+        if ($retreats->count() >= 1) {
+            foreach ($retreats as $retreat) {
+                $automaticSuccessMessage = "Automatic confirmation email has been sent for retreat #".$retreat->idnumber.".";
+                $automaticErrorMessage = "Automatic confirmation email failed to send for retreat #".$retreat->idnumber.".";
 
-			foreach ($retreats as $retreat) {
-				$registrations = $retreat->registrations()
-					->where('canceled_at', null)
-					->where('status_id', config('polanco.registration_status_id.registered'))
-					->with('contact')
-					->whereHas('contact', function($query) {
-						$query->where('do_not_email', 0);
-					})
-					->with('contact.touchpoints')
-					->whereDoesntHave('contact.touchpoints', function($query) use ($automaticSuccessMessage){
-						$query->where('notes', 'like', $automaticSuccessMessage);
-					})
-					->get();
+                $registrations = $retreat->registrations()
+                    ->where('canceled_at', null)
+                    ->where('status_id', config('polanco.registration_status_id.registered'))
+                    ->with('contact')
+                    ->whereHas('contact', function($query) {
+                        $query->where('do_not_email', 0);
+                    })
+                    ->with('contact.touchpoints')
+                    ->whereDoesntHave('contact.touchpoints', function($query) use ($automaticSuccessMessage){
+                        $query->where('notes', 'like', $automaticSuccessMessage);
+                    })
+                    ->get();
 
-				foreach ($registrations as $registration) {
-					$primaryEmail = $registration->retreatant->email_primary_text;
+                foreach ($registrations as $registration) {
+                    $primaryEmail = $registration->retreatant->email_primary_text;
 
-					// For automatic emails, remember_token must be set for all participants in retreat. 
-					if (!$registration->remember_token) {
-						$registration->remember_token = str_random(60);
-						$registration->save();
-					}
+                    // For automatic emails, remember_token must be set for all participants in retreat. 
+                    if (!$registration->remember_token) {
+                        $registration->remember_token = str_random(60);
+                        $registration->save();
+                    }
 
-					$alfonso = \App\Contact::where('display_name', 'Juan Alfonso de Polanco')->first();
+                    $alfonso = \App\Contact::where('display_name', 'Juan Alfonso de Polanco')->first();
 
-					$touchpoint = new \App\Touchpoint();
-					$touchpoint->person_id = $registration->contact->id;
-					$touchpoint->staff_id = $alfonso->id;
-					$touchpoint->touched_at = Carbon::now();
-					$touchpoint->type = 'Email';
+                    $touchpoint = new \App\Touchpoint();
+                    $touchpoint->person_id = $registration->contact->id;
+                    $touchpoint->staff_id = $alfonso->id;
+                    $touchpoint->touched_at = Carbon::now();
+                    $touchpoint->type = 'Email';
 
-					try {
-						$this->mailer->to($primaryEmail)->queue(new RetreatConfirmation($retreatant));
-						$touchpoint->notes = $automaticSuccessMessage;
-						$touchpoint->save();
-					} catch ( \Exception $e ) {
-						$touchpoint->notes = $automaticErrorMessage;
-						$touchpoint->save();
-					}
-				}
-			}
-		}
-	}
+                    try {
+                        $this->mailer->to($primaryEmail)->queue(new RetreatConfirmation($registration));
+                        $touchpoint->notes = $automaticSuccessMessage;
+                        $touchpoint->save();
+                    } catch ( \Exception $e ) {
+                        $touchpoint->notes = $automaticErrorMessage;
+                        $touchpoint->save();
+                    }
+                }
+            }
+        }
+    }
 }
