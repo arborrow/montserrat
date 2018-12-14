243a56f67ff343cd5f97002544bc4f8c5b63fa1a
Ata Gowani <atagowani@gmail.com>
2018-12-13 23:55:38 -0600

    Update UI for person create page

--- resources/views/persons/create.blade.php
+++ resources/views/persons/create.blade.php
@@ -1,150 +1,226 @@
 @extends('template')
 @section('content')
 
+<div class="row bg-cover">
+    <div class="col-12">
+        <h2>Create Person</h2>
+    </div>
+    <div class="col-12">
+        {!! Form::open(['url' => 'person', 'files' => 'true', 'method' => 'post', 'class' => 'form-horizontal panel']) !!}
+            <div class="row">
+                <div class="col-12">
+                    <h3>Basic Information</h3>
+                </div>
+                <div class="col-12">
+                    <div class="form-group">
+                        <div class="row">
+                            <div class="col-3">
+                                {!! Form::label('prefix_id', 'Title: ') !!}
+                                {!! Form::select('prefix_id', $prefixes, 0, ['class' => 'form-control']) !!}
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-4">
+                                {!! Form::label('first_name', 'First: ')  !!}
+                                {!! Form::text('first_name', null, ['required'=>'', 'class' => 'form-control','oninvalid'=>"this.setCustomValidity('First name required')"]) !!}
+                            </div>
+                            <div class="col-4">
+                                {!! Form::label('middle_name', 'Middle: ')  !!}
+                                {!! Form::text('middle_name', null, ['class' => 'form-control']) !!}
+                            </div>
+                            <div class="col-4">
+                                {!! Form::label('last_name', 'Last: ')  !!}
+                                {!! Form::text('last_name', null, ['required'=>'','class' => 'form-control']) !!}
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-3">
+                                {!! Form::label('suffix_id', 'Suffix: ')  !!}
+                                {!! Form::select('suffix_id', $suffixes, 0, ['class' => 'form-control']) !!}
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-4">
+                                {!! Form::label('nick_name', 'Nickname: ') !!}
+                                {!! Form::text('nick_name', null, ['class' => 'form-control']) !!}
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-3">
+                                {!! Form::label('contact_type', 'Contact type: ')  !!}
+                                {!! Form::select('contact_type', $contact_types, 1, ['class' => 'form-control']) !!}
+                            </div>
+                            <div class="col-3">
+                                {!! Form::label('subcontact_type', 'Subcontact type: ')  !!}
+                                {!! Form::select('subcontact_type', $subcontact_types, 0, ['class' => 'form-control']) !!}
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-3">
+                                @can('create-avatar')
+                                    {!! Form::label('avatar', 'Picture (max 5M): ')  !!}
+                                    {!! Form::file('avatar', ['class' => 'form-control-file']); !!}
+                                @endCan 
+                            </div>
+                        </div>    
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-12">
+                    <h3>Emergency Contact Information</h3>
+                </div>
+                <div class="col-12">
+                    <div class="form-group">
+                        <div class="row">
+                            <div class="col-4">
+                                {!! Form::label('emergency_contact_name', 'Name: ') !!}
+                                {!! Form::text('emergency_contact_name', null, ['class' => 'form-control']) !!}
+                            </div>
+                            <div class="col-4">
+                                {!! Form::label('emergency_contact_relationship', 'Relationship: ')  !!}
+                                {!! Form::text('emergency_contact_relationship', null, ['class' => 'form-control']) !!}
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-4">
+                                {!! Form::label('emergency_contact_phone', 'Phone: ')  !!}
+                                {!! Form::text('emergency_contact_phone', null, ['class' => 'form-control']) !!}
+                            </div>
+                            <div class="col-4">
+                                {!! Form::label('emergency_contact_phone_alternate', 'Alt. Phone: ')  !!}
+                                {!! Form::text('emergency_contact_phone_alternate', null, ['class' => 'form-control']) !!}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-12">
+                    <h3>Addresses</h3>
+                </div>
+                <div class="col-12">
+                    @include('persons.create.addresses')
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-12">
+                    <h3>Phone Numbers</h3>
+                </div>
+                <div class="col-12">
+                    @include('persons.create.phones')
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-12">
+                    <h3>Email Addresses</h3>
+                </div>
+                <div class="col-12">
+                    @include('persons.create.emails')
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-12">
+                    <h3>Websites (URLs)</h3>
+                </div>
+                <div class="col-12">
+                    @include('persons.create.urls')
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-12">
+                    <h3>Demographics</h3>
+                </div>
+                <div class="col-12">
+                    <div class="form-group">
+                        <div class="row">
+                            <div class="col-4">
+                                {!! Form::label('gender_id', 'Gender:')  !!}
+                                {!! Form::select('gender_id', $genders, 0, ['class' => 'form-control']) !!}
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-4">
+                                {!! Form::label('birth_date', 'Birth Date:') !!}
+                                {!! Form::text('birth_date', null, ['class'=>'form-control','data-provide'=>'datepicker', 'autocomplete' => 'off']) !!}
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-4">
+                                {!! Form::label('religion_id', 'Religion:') !!}
+                                {!! Form::select('religion_id', $religions, 1, ['class' => 'form-control']) !!} 
+                            </div>
+                            <div class="col-4">
+                                {!! Form::label('occupation_id', 'Occupation:')  !!}
+                                {!! Form::select('occupation_id', $occupations, 0, ['class' => 'form-control']) !!} 
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-4">
+                                {!! Form::label('parish_id', 'Parish:') !!}
+                                {!! Form::select('parish_id', $parish_list, 0, ['class' => 'form-control']) !!} 
+                            </div>
+                            <div class="col-4">
+                                {!! Form::label('ethnicity_id', 'Ethnicity:')  !!}
+                                {!! Form::select('ethnicity_id', $ethnicities, 'Unspecified', ['class' => 'form-control']) !!}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-12">
+                    <h3>Languages</h3>
+                </div>
+                <div class="col-12">
+                    <div class="form-group">
+                        <div class="row">
+                            <div class="col-4">
+                                {!! Form::label('languages', 'Languages:') !!}
+                                {!! Form::select('languages[]', $languages, 45, ['id' => 'languages', 'class' => 'form-control select2', 'multiple' => 'multiple']) !!}
+                            </div>
+                            <div class="col-4">
+                                {!! Form::label('preferred_language_id', 'Preferred Language:')  !!}
+                                {!! Form::select('preferred_language_id', $languages, 45, ['class' => 'form-control']) !!}
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-4">
+                                {!! Form::label('referrals', 'Referrals:')  !!}
+                                {!! Form::select('referrals[]', $referrals, NULL, ['id' => 'referrals', 'class' => 'form-control select2', 'multiple' => 'multiple']) !!}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        {!! Form::close() !!}
+    </div>
+</div>
+
 <section class="section-padding">
     <div class="jumbotron text-left">
-        <h2><strong>Create Person</strong></h2>
-        {!! Form::open(['url' => 'person', 'files' => 'true', 'method' => 'post', 'class' => 'form-horizontal panel']) !!}
-
-        <div class='row'>
-            <div class='col-md-8'>
-                <span>
-                <h2>Name</h2>
-                    <div class="form-group">
-                        {!! Form::label('prefix_id', 'Title: ', ['class' => 'col-md-1'])  !!}
-                        {!! Form::select('prefix_id', $prefixes, 0, ['class' => 'col-md-2']) !!}
-                        <div class="clearfix"> </div>
-                        {!! Form::label('first_name', 'First: ', ['class' => 'col-md-1'])  !!}
-                        {!! Form::text('first_name', null, ['required'=>'', 'class' => 'col-md-2','oninvalid'=>"this.setCustomValidity('First name required')"]) !!}
-                        {!! Form::label('middle_name', 'Middle: ', ['class' => 'col-md-1'])  !!}
-                        {!! Form::text('middle_name', null, ['class' => 'col-md-2']) !!}
-                        {!! Form::label('last_name', 'Last: ', ['class' => 'col-md-1'])  !!}
-                        {!! Form::text('last_name', null, ['required'=>'','class' => 'col-md-2']) !!}
-                        <div class="clearfix"> </div>
-                        {!! Form::label('suffix_id', 'Suffix: ', ['class' => 'col-md-1'])  !!}
-                        {!! Form::select('suffix_id', $suffixes, 0, ['class' => 'col-md-2']) !!}
-                        <div class="clearfix"> </div>
-                        {!! Form::label('nick_name', 'Nick: ', ['class' => 'col-md-1'])  !!}
-                        {!! Form::text('nick_name', null, ['class' => 'col-md-2']) !!}
-                        <div class="clearfix"> </div>
-                        
-                            {!! Form::label('contact_type', 'Contact type: ', ['class' => 'col-md-3'])  !!}
-                            {!! Form::select('contact_type', $contact_types, 1, ['class' => 'col-md-2']) !!}
-                        <div class="clearfix"> </div>
-                            {!! Form::label('subcontact_type', 'Subcontact type: ', ['class' => 'col-md-3'])  !!}
-                            {!! Form::select('subcontact_type', $subcontact_types, 0, ['class' => 'col-md-2']) !!}
-                        <div class="clearfix"> </div>
-                        @can('create-avatar')
-                            {!! Form::label('avatar', 'Picture (max 5M): ', ['class' => 'col-md-3'])  !!}
-                            {!! Form::file('avatar'); !!}
-                        @endCan        
-                    </div>
-                        
-                </span>
-            </div>
-        </div>
-        <div class="clearfix"> </div>
-        <div class='row'>
-            <div class='col-md-8' style="background-color: lightcoral;">
-                <span>
-                    <h2>Emergency Contact Information</h2>
-                    <div class="form-group">
-                        {!! Form::label('emergency_contact_name', 'Name: ', ['class' => 'col-md-1'])  !!}
-                        {!! Form::text('emergency_contact_name', null, ['class' => 'col-md-2']) !!}
-                        {!! Form::label('emergency_contact_relationship', 'Relationship: ', ['class' => 'col-md-2'])  !!}
-                        {!! Form::text('emergency_contact_relationship', null, ['class' => 'col-md-2']) !!}
-                        <div class="clearfix"> </div>
-                        {!! Form::label('emergency_contact_phone', 'Phone: ', ['class' => 'col-md-1'])  !!}
-                        {!! Form::text('emergency_contact_phone', null, ['class' => 'col-md-2']) !!}
-                        {!! Form::label('emergency_contact_phone_alternate', 'Alt. Phone: ', ['class' => 'col-md-2'])  !!}
-                        {!! Form::text('emergency_contact_phone_alternate', null, ['class' => 'col-md-2']) !!}
-                    </div>
-                </span>
-            </div>
-        </div>
-        <div class="clearfix"> </div>
-        <div class='row'>
-            <div class='col-md-12'>
-                <span>
-                    @include('persons.create.addresses')
-                </span>
-            </div>
-        </div>
-        <div class="clearfix"> </div>
-
-        <div class='row'>
-            <div class='col-md-8'>
-                <span>
-                    @include('persons.create.phones')
-                </span>
-            </div>
-        </div>
-        <div class="clearfix"> </div>
-
-
      <div class='row'>
-            <div class='col-md-8'>
-                <span>
-                    @include('persons.create.emails')
-                </span>
-            </div>
-        </div>
-        <div class="clearfix"> </div>
-
-     <div class='row'>
-        <div class='col-md-8'>
-            <span>
-                @include('persons.create.urls')
-            </span>
-        </div>
-    </div>
-    <div class="clearfix"> </div>
-       
         <div class='row'>
             <div class='col-md-8'>
                 <span>
-                    <h2>Demographics</h2>
                     <div class="form-group">
-                        {!! Form::label('gender_id', 'Gender:', ['class' => 'col-md-2'])  !!}
-                        {!! Form::select('gender_id', $genders, 0, ['class' => 'col-md-2']) !!}
-                        <div class="clearfix"> </div>
-                        {!! Form::label('birth_date', 'Birth Date:', ['class' => 'col-md-2']) !!}
-                        {!! Form::text('birth_date', null, ['class'=>'col-md-2','data-provide'=>'datepicker']) !!}
-                        <div class="clearfix"> </div>
-
+                        
                         
                     </div>
-                    <div class="form-group">
-                        {!! Form::label('religion_id', 'Religion:', ['class' => 'col-md-2'])  !!}
-                        {!! Form::select('religion_id', $religions, 1, ['class' => 'col-md-2']) !!} 
-                        {!! Form::label('occupation_id', 'Occupation:', ['class' => 'col-md-2'])  !!}
-                        {!! Form::select('occupation_id', $occupations, 0, ['class' => 'col-md-3']) !!} 
-                        <div class="clearfix"> </div>
-                        
-                        {!! Form::label('parish_id', 'Parish:', ['class' => 'col-md-2'])  !!}
-                        {!! Form::select('parish_id', $parish_list, 0, ['class' => 'col-md-8']) !!} 
-                        </div>
-                    <div class="form-group">                        
-                        {!! Form::label('ethnicity_id', 'Ethnicity:', ['class' => 'col-md-2'])  !!}
-                        {!! Form::select('ethnicity_id', $ethnicities, 'Unspecified', ['class' => 'col-md-2']) !!}
-                        <div class="clearfix"> </div>
-                        {!! Form::label('languages', 'Languages:', ['class' => 'col-md-2'])  !!}
-                        {!! Form::select('languages[]', $languages, 45, ['id' => 'languages', 'class' => 'form-control col-md-2','multiple' => 'multiple','style'=>'width: auto; font-size: inherit;']) !!}
-                         <div class="clearfix"> </div>
-                       
-                        {!! Form::label('preferred_language_id', 'Preferred Language:', ['class' => 'col-md-2'])  !!}
-                        {!! Form::select('preferred_language_id', $languages, 45, ['class' => 'col-md-3']) !!}
-                        <div class="clearfix"> </div>
-                       
-                        {!! Form::label('referrals', 'Referrals:', ['class' => 'col-md-2'])  !!}
-                        {!! Form::select('referrals[]', $referrals, NULL, ['id' => 'referrals', 'class' => 'form-control col-md-2','multiple' => 'multiple','style'=>'width: auto; font-size: inherit;']) !!}
-                        
-                    </div>
-                    {!! Form::label('deceased_date', 'Deceased Date:', ['class' => 'col-md-3'])  !!}
+                        {!! Form::label('deceased_date', 'Deceased Date:', ['class' => 'col-md-3'])  !!}
                         {!! Form::text('deceased_date', null, ['class'=>'col-md-2','data-provide'=>'datepicker']) !!}
                         {!! Form::label('is_deceased', 'Is Deceased:', ['class' => 'col-md-2'])  !!}
                         {!! Form::checkbox('is_deceased', 1, false,['class' => 'col-md-1']) !!}
-                        <div class="clearfix"> </div>
+                    <div class="clearfix"> 
+
+                    </div>
 
                 </span>
             </div>
@@ -240,7 +316,7 @@
                     <div class="form-group">
                         {!! Form::submit('Add Person', ['class'=>'btn btn-default']) !!}
                     </div>
-                    {!! Form::close() !!}
+                    
                 </div>
             </div>
             <div class="clearfix"> </div>
