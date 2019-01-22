9fb8c714b7f23a87a05772a0d54c67c8148230d9
Ata Gowani <atagowani@gmail.com>
2019-01-22 10:20:05 -0600

    Revert confirmDelete

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -2,11 +2,3 @@ $(document).ready(function() {
   $('.select2').select2(); // Apply select2
   $( "#datepicker" ).datepicker();
 });
-
-function ConfirmDelete() {
-  var x = confirm("Are you sure you want to delete?");
-  if (x)
-    return true;
-  else
-    return false;
-}
