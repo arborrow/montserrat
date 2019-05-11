1f21673853e981336f61eae09fc116e6164ad1dc
Anthony Borrow <anthony@moodle.org>
2019-05-10 21:26:19 -0500

    clean up URL paths to make relative to wwwroot in AGC blade - #227

--- resources/views/donations/agc.blade.php
+++ resources/views/donations/agc.blade.php
@@ -26,21 +26,21 @@
                 <tbody>
                     @foreach($donations as $donation)
                         <tr>
-                            <td><a href="/donation/{{ $donation->donation_id}}">{{ date('M d, Y', strtotime($donation->donation_date)) }}</a></td>
+                            <td><a href='{{url("donation/".$donation->donation_id)}}'"> {{ date('M d, Y', strtotime($donation->donation_date)) }} </a></td>
                             <td>{!! $donation->contact->contact_link_full_name ?? 'Unknown contact' !!} ({{$donation->contact->agc_household_name}}) </td>
                             <td>{{ $donation->donation_description }} </td>
                             <td>${{number_format($donation->payments_paid,2)}}/${{ number_format($donation->donation_amount,2) }}</td>
                             <td>
                                 @if(isset($donation['Thank You']))
-                                    <a href="/donation/{{$donation->donation_id}}/agcacknowledge">{{$donation['Thank You']}}</a>
+                                    <a href='{{url("donation/".$donation->donation_id."/agcacknowledge") }}'>{{$donation['Thank You']}}</a>
                                 @else
                                     @if ($donation->percent_paid == 100)
-                                        <a href="/donation/{{$donation->donation_id}}/agcacknowledge"><img src="/images/letter.png" alt="Print acknowledgement" title="Print acknowledgement"></a>
+                                        <a href='{{ url("donation/".$donation->donation_id."/agcacknowledge") }}'><img src='{{ url("images/letter.png") }}' alt="Print acknowledgement" title="Print acknowledgement"></a>
                                     @else
                                         Awaiting full payment
                                     @endIf
                                 @endIf
-                                <a href="/person/{{$donation->contact_id}}/envelope10"><img src="/images/envelope.png" alt="Print envelope" title="Print envelope"></a>
+                                <a href='{{ url("person/".$donation->contact_id."/envelope10") }}'><img src='{{ url("images/envelope.png") }}' alt="Print envelope" title="Print envelope"></a>
                             </td>
                         </tr>
                     @endforeach
