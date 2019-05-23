31e71bb4fda7f4dd7ab4b31fa48ae1838d8e8b47
Ata Gowani <atagowani@gmail.com>
2019-05-23 12:23:58 -0500

    Update formatting

--- resources/views/emails/error.blade.php
+++ resources/views/emails/error.blade.php
@@ -1,17 +1,22 @@
-<h1>Polanco Error Detected</h1>
+<h1>{{ config('polanco.site_name') }} {{ $error->getStatusCode() }} Error Detected</h1>
+
+<h4>User Details</h4>
+<p>Username: {{ $user }}</p>
+<p>URL: {{ $url }}</p>
+<p>IP: {{ $ip }}</p>
 
 <h4>Message</h4>
 @if ($error->getMessage())
-  {{!! $error->getMessage() !!}}
+  {{ $error->getMessage() }}
 @else
   <p>No message.</p>
 @endif
 
 <h4>Error Code</h4>
-{{!! $error->getCode() !!}}
+{{ $error->getCode() }}
 
 <h4>Occurred At</h4>
-{{!! $error->getFile() !!}}
+{{ $error->getFile() }}
 
 <h4>Error as String</h4>
-{{!! $error->__toString() !!}}
+{{ $error }}
