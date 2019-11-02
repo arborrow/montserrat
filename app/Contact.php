58d25582c2fa4a94d21e1568d8ac12bf49af1c7a
Anthony Borrow <anthony@moodle.org>
2019-11-02 17:13:30 -0500

    fix typo

--- app/Contact.php
+++ app/Contact.php
@@ -778,7 +778,7 @@ class Contact extends Model
     }
     public function groups()
     {
-        return $this->hasMany(GroupContact::class, 'contact_id', 'id')->whereStaus("Added");
+        return $this->hasMany(GroupContact::class, 'contact_id', 'id')->whereStatus("Added");
     }
     public function languages()
     {
