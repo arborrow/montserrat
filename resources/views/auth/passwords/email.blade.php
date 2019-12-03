c2aabe70e164b20ef97760433bf12c5b0fdd879f
Laravel Shift <shift@laravelshift.com>
2019-12-03 16:27:40 +0000

    Streamline Blade directives
    Laravel offers many built-in Blade directives, including: `@auth`, `@guest` ,`@csrf`, `@method`, `@json`, and more.
    
    Review the [Blade][1] documentation for more details.
    
    [1]: https://laravel.com/docs/5.7/blade

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -14,7 +14,7 @@
                     @endif
 
                     <form class="form-horizontal" role="form" method="POST" action="{{ route('password.email') }}">
-                        {{ csrf_field() }}
+                        @csrf
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
                             <label for="email" class="col-md-4 control-label">E-Mail Address</label>
