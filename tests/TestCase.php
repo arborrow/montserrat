5bdbc43dddd2cac8f5af162df2928ac3551dc540
Jason McCreary <jason@pureconcepts.net>
2019-12-17 21:19:54 -0600

    Extract method for sharing

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,4 +8,22 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication, HttpTestAssertions;
+
+    protected function createUserWithPermission(string $permission, array $data = [])
+    {
+        $reference = \App\Permission::where('name', $permission)->first();
+        if (is_null($reference)) {
+            throw new \InvalidArgumentException('permission does not exist: ' . $reference);
+        }
+
+        $role = \App\Role::where('name', 'test-role:' . $permission)->first();
+        if (is_null($role)) {
+            throw new \InvalidArgumentException('A test role for the permission (' . $permission. ') does not exist. Did you run the seeder?');
+        }
+
+        $user = factory(\App\User::class)->create($data);
+        $user->assignRole($role->name);
+
+        return $user;
+    }
 }
