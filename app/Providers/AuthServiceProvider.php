2e1cffdc1e27f99488c033f3a5fffa855de09d5b
Jason McCreary <jason@pureconcepts.net>
2019-12-17 21:14:47 -0600

    Remove dead code

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -14,7 +14,6 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        'App\\Model' => 'App\\Policies\\ModelPolicy',
         \App\Attachment::class => \App\Policies\AttachmentPolicy::class,
 
     ];
