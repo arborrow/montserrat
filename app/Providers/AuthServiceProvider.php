84c99b466f876072581ccbae0b152a8de6d02397
Jason McCreary <jason@pureconcepts.net>
2019-12-17 21:15:56 -0600

    Remove gaurd for testing environment

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -27,23 +27,21 @@ class AuthServiceProvider extends ServiceProvider
     {
         parent::registerPolicies();
 
-        //prior to installing the app ignore checking for superuser or permissions to avoid artisan errors about missing permissions table
-        if (NULL !== config('app.key')) {
-            Gate::before(function ($user) {
-                $superuser = \App\Permission::whereName('superuser')->firstOrFail();
-                // only return true if this user has a role with the superuser permission
-                // otherwise
-                if ($user->hasRole($superuser->roles)) {
-                    return true;
-                }
-            });
+        Gate::before(function ($user) {
+            $superuser = \App\Permission::where('name', 'superuser')->firstOrFail();
 
-            $permissions = Permission::with('roles')->get();
-            foreach ($permissions as $permission) {
-                Gate::define($permission->name, function ($user) use ($permission) {
-                    return $user->hasRole($permission->roles);
-                });
+            // only return true if this user has a role with the superuser permission
+            // otherwise
+            if ($user->hasRole($superuser->roles)) {
+                return true;
             }
+        });
+
+        $permissions = Permission::with('roles')->get();
+        foreach ($permissions as $permission) {
+            Gate::define($permission->name, function ($user) use ($permission) {
+                return $user->hasRole($permission->roles);
+            });
         }
     }
 }
