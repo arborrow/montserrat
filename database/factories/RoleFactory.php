f9661257526cf83000eddcba1b0ddc56e09e3283
Jason McCreary <jason@pureconcepts.net>
2019-12-17 21:16:19 -0600

    Regenerate factories

--- database/factories/RoleFactory.php
+++ database/factories/RoleFactory.php
@@ -6,5 +6,8 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Role::class, function (Faker $faker) {
     return [
+        'name' => $faker->name,
+        'display_name' => $faker->word,
+        'description' => $faker->text,
     ];
 });
