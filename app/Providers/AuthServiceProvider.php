0a58a51ab5c2e00a6246602b9cea3f619d6b0af0
Anthony Borrow <anthony@moodle.org>
2019-12-22 16:44:53 -0600

    adding back in check that the database has yet to be configured; updating README

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -27,21 +27,24 @@ class AuthServiceProvider extends ServiceProvider
     {
         parent::registerPolicies();
 
-        Gate::before(function ($user) {
-            $superuser = \App\Permission::where('name', 'superuser')->firstOrFail();
+        //prior to installing the app ignore checking for superuser or permissions to avoid artisan errors about missing permissions table
+        if (NULL !== config('app.key')) {
+            Gate::before(function ($user) {
+                $superuser = \App\Permission::where('name', 'superuser')->firstOrFail();
 
-            // only return true if this user has a role with the superuser permission
-            // otherwise
-            if ($user->hasRole($superuser->roles)) {
-                return true;
+                  // only return true if this user has a role with the superuser permission
+                  // otherwise
+                  if ($user->hasRole($superuser->roles)) {
+                    return true;
+                  }
+                });
+
+            $permissions = Permission::with('roles')->get();
+            foreach ($permissions as $permission) {
+                Gate::define($permission->name, function ($user) use ($permission) {
+                    return $user->hasRole($permission->roles);
+                });
             }
-        });
-
-        $permissions = Permission::with('roles')->get();
-        foreach ($permissions as $permission) {
-            Gate::define($permission->name, function ($user) use ($permission) {
-                return $user->hasRole($permission->roles);
-            });
         }
     }
 }
