39128f47f248a421d73e49afbb5d9a791aca6c20
Anthony Borrow <anthony@moodle.org>
2020-01-01 17:27:41 -0600

    some minor tweaks

--- database/factories/AttachmentFactory.php
+++ database/factories/AttachmentFactory.php
@@ -6,5 +6,14 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Attachment::class, function (Faker $faker) {
     return [
+        'file_type_id' => null,
+        'mime_type' => null,
+        'uri' => null,
+        'document' => null,
+        'description' => $faker->sentence,
+        'upload_date' => $faker->dateTime(),
+        'remember_token' => Str::random(10),
+        'entity' => null,
+        'entity_id' => $faker->randomDigit,
     ];
 });
