ad9202debb84b82a9b64797d3ceca80ded168f53
Anthony Borrow <anthony@moodle.org>
2020-03-06 19:08:20 -0600

    adding dashboard descriptions for revenue tracking

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -57,9 +57,10 @@ class DashboardController extends Controller
             $prev_year = $year->copy()->subYear();
 
             $agc_donors = \App\Donation::orderBy('donation_date', 'desc')->whereIn('donation_description', ['Annual Giving', 'Endowment', 'Scholarship', 'Buildings & Maintenance'])->where('donation_date', '>=', $prev_year->year.'-07-01')->where('donation_date', '<', $year->year.'-07-01')->groupBy('contact_id')->get();
+            $agc_donations = \App\Donation::orderBy('donation_date', 'desc')->whereIn('donation_description', ['Annual Giving', 'Endowment', 'Scholarship', 'Buildings & Maintenance'])->where('donation_date', '>=', $prev_year->year.'-07-01')->where('donation_date', '<', $year->year.'-07-01')->get();
 
-            $donors[$label]['count'] = $agc_donors->count();
-            $donors[$label]['sum'] = $agc_donors->sum('donation_amount');
+            $donors[$label]['count'] = $agc_donors->count(); //unique donors
+            $donors[$label]['sum'] = $agc_donations->sum('donation_amount');
 
         }
 
@@ -85,4 +86,54 @@ class DashboardController extends Controller
 
         return view('dashboard.index', compact('chart','agc_donor_chart','agc_amount'));
     }
+
+    public function donation_description_chart($category = null) {
+        switch ($category) {
+            case 'book' : $donation_description = 'Books'; break;
+            case 'deposit' : $donation_description = 'Deposit'; break;
+            case 'diocese' : $donation_description = 'Diocesan/Provincial/Parish Retreats'; break;
+            case 'donation' : $donation_description = 'Donation'; break;
+            case 'flower' : $donation_description = 'Flowers and Landscape'; break;
+            case 'gift' : $donation_description = 'Gift Certificate Purchase'; break;
+            case 'other' : $donation_description = 'Other Retreats'; break;
+            case 'offering' : $donation_description = 'Retreat Offering'; break;
+            case 'tip' : $donation_description = 'Tips'; break;
+            default : $donation_description = 'Retreat Offering';
+        }
+        // dd($donation_description);
+        $current_year = (date('m') > 6) ? date('Y') + 1 : date('Y');
+        $number_of_years = 5;
+        $years = array();
+        for ($x = -$number_of_years; $x <= 0; $x++) {
+            $today = \Carbon\Carbon::now();
+            $today->day = 1;
+            $today->month = 1;
+            $today->year = $current_year;
+            $years[$x] = $today->addYear($x);
+        }
+
+        foreach ($years as $year) {
+            $label = $year->year;
+            $prev_year = $year->copy()->subYear();
+            $donations =  \App\Donation::whereDonationDescription($donation_description)->where('donation_date', '>=', $prev_year->year.'-07-01')->where('donation_date', '<', $year->year.'-07-01')->get();
+            $donors[$label]['count'] = $donations->count();
+            $donors[$label]['sum'] = $donations->sum('donation_amount');
+
+        }
+
+        $average_amount = (array_sum(array_column($donors,'sum'))/$number_of_years+1);
+        foreach ($years as $year) {
+            $label = $year->year;
+            $prev_year = $year->copy()->subYear();
+            $donors[$label]['average_amount'] = $average_amount;
+        }
+        $donation_description_chart = new RetreatOfferingChart;
+        $donation_description_chart->labels(array_keys($donors));
+        $donation_description_chart->dataset('Donations for '.$donation_description, 'line', array_column($donors,'sum'));
+        $donation_description_chart->dataset('Average', 'line', array_column($donors,'average_amount'));
+
+        $descriptions = array(['book','deposit','diocese','donation','flower','gift','offering','tip']);
+        return view('dashboard.description', compact('donation_description_chart','descriptions'));
+    }
+
 }
---
+++ resources/views/dashboard/description.blade.php
@@ -0,0 +1,38 @@
+@extends('template')
+@section('content')
+
+    <section class="section-padding">
+        <div class="jumbotron text-left">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h1>
+                        <span class="grey">Donation Description Dashboard</span>
+                    </h1>
+                </div>
+                    <div class="row">
+                        <div class="col-md-4 col-12">
+                            <select class="custom-select" onchange="this.options[this.selectedIndex].value && (window.location = this.options[this.selectedIndex].value);">
+                                <option value="">Filter by Donation Description ...</option>
+                                @foreach($descriptions as $description)
+                                    <option value="{{url('dashboard/description/book')}}">Books</option>
+                                    <option value="{{url('dashboard/description/deposit')}}">Deposits</option>
+                                    <option value="{{url('dashboard/description/diocese')}}">Diocesan events</option>
+                                    <option value="{{url('dashboard/description/donation')}}">Donations</option>
+                                    <option value="{{url('dashboard/description/flower')}}">Flowers</option>
+                                    <option value="{{url('dashboard/description/gift')}}">Gift Certificates</option>
+                                    <option value="{{url('dashboard/description/offering')}}">Retreat Offerings</option>
+                                    <option value="{{url('dashboard/description/other')}}">Other events</option>
+                                    <option value="{{url('dashboard/description/tip')}}">Tips</option>
+                                @endForeach
+                            </select>
+                        </div>
+                    </div>
+
+
+                <div> {!! $donation_description_chart->container() !!} </div>
+            </div>
+        </div>
+    </section>
+    {!! $donation_description_chart->script() !!}
+
+@stop
