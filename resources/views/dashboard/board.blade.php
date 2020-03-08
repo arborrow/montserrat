8055e0eed45825d52866d26db23687cf51496160
Anthony Borrow <anthony@moodle.org>
2020-03-07 23:25:35 -0600

    trivial css fixes to dashboard.board blade

--- resources/views/dashboard/board.blade.php
+++ resources/views/dashboard/board.blade.php
@@ -35,8 +35,8 @@
                             <th>Avg.$/Person/Night</th>
                         </thead>
                         @foreach($board_summary as $category)
-                        <tr>
-                            <td>{{ $category->type }}</td>
+                        <tr style='text-align: right'>
+                            <td style='text-align: left; font-weight: bold;'>{{ $category->type }}</td>
                             <td>${{ number_format($category->total_pledged,2) }}</td>
                             <td>${{ number_format($category->total_paid,2) }}</td>
                             <td>{{ $category->total_participants }}</td>
@@ -45,8 +45,8 @@
                             <td>${{ ($category->total_pn>0) ? (number_format(($category->total_paid/$category->total_pn),2)) : '0.00' }}
                         </tr>
                         @endforeach
-                        <tr style='font-weight: bold;'>
-                            <td>Total</td>
+                        <tr style='text-align: right; font-weight: bold;'>
+                            <td style='text-align: left;'>Total</td>
                             <td>${{ number_format(array_sum(array_column($board_summary,'total_pledged')),2) }}</td>
                             <td>${{ number_format(array_sum(array_column($board_summary,'total_paid')),2) }}</td>
                             <td>{{ number_format(array_sum(array_column($board_summary,'total_participants')),0) }}</td>
