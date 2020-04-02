f3e56c206a8a84a9c3dd6ef2d51fcd58fbbcd92d
Anthony Borrow <anthony@moodle.org>
2020-04-02 14:26:47 -0500

    updating birthday email

--- resources/views/emails/retreatant-birthday.blade.php
+++ resources/views/emails/retreatant-birthday.blade.php
@@ -3,19 +3,43 @@
 <head>
 	<title></title>
 </head>
-<body> 
+<body>
 	<div dir="ltr"><u></u>
 		<div>
+			<p style="text-align:center">
+				<b>&quot;I have told you this <br>
+					so that my joy may be in you <br>
+					and your joy may be complete!&quot; <br>
+					<span style="font-size:8pt">
+						John 15:11
+					</span>
+ 				</b>
+			</p>
+			<h2>
+				<img src="https://polanco.montserratretreat.org/images/happy_birthday.jpg" style="display:block;margin-left:auto;margin-right:auto" width="350" height="403">
+			</h2>
+
+			<p style="text-align:center">
+				<span style="font-size:10pt">
+					The Jesuits and Staff of <br>
+					Montserrat Jesuit Retreat House <br>
+					wish you many joy-filled birthday blessings. <br><br>
+					May your arms and heart <br>
+					open wide to receive all <br>
+					of God's good gifts.
+				</span>
+			</p>
+
+			<p style="text-align:center">
+				<span style="font-size:10pt">
+					We look forward to the joy of welcoming you back to Montserrat!
+				</span>
+			</p>
+
 			<h2>
 				<a href="https://www.montserratretreat.org" target="_blank"> <img src="https://i.imgur.com/c5kmJcn.jpg" style="display:block;margin-left:auto;margin-right:auto" width="139" height="44"></a>
 			</h2>
-			<h2>
-				<img src="https://i.imgur.com/XULzS27.jpg" style="display:block;margin-left:auto;margin-right:auto" width="326" height="217">
-			</h2>
-			<p style="text-align:center"><b>You are the light of the world! </b><br><b> <span style="font-size:8pt">Matthew 5:14</span></b></p>
-			<p style="text-align:center"><span style="font-size:10pt">The Jesuits and Staff of <br>Montserrat Jesuit Retreat House <br>wish you many birthday blessings. <br>May your light shine brightly now and always! </span></p>
-			<p style="text-align:center"><span style="font-size:10pt">We look forward to welcoming you to Montserrat again very soon!</span> <span style="font-size:8pt"> </span></p>
-			<p style="text-align:center"><span style="font-size:8pt"><br><i><a href="tel:(940)%20321-6020" value="+19403216020" target="_blank">940-321-6020</a> . <a href="https://www.montserratretreat.org/" target="_blank">montserratretreat.org</a> </i></span></p>
+			<p style="text-align:center"><span style="font-size:8pt"><br><i><a href="tel:(940) 321-6020" value="+19403216020" target="_blank">940-321-6020</a> . <a href="https://www.montserratretreat.org/" target="_blank">montserratretreat.org</a> </i></span></p>
 		</div>
 	</div>
 
