8e5fe263ca7fe64325290d8661209e5438ff052e
Anthony Borrow <arborrow@jesuits.net>
2020-07-24 16:44:35 -0500

    sort retreatant roster report by retreatant.sort_name, readd registration notes

--- resources/views/reports/retreatroster.blade.php
+++ resources/views/reports/retreatroster.blade.php
@@ -14,8 +14,9 @@
         <th class="row-4 row-room_preference" style='width: 20%'>Room Preference</th>
         <th class="row-2 row-note" style='width: 25%'>Health Notes</th>
         <th class="row-5 row-dietary" style='width: 20%'>Dietary Preference</th>
+        <th class="row-5 row-note" style='width: 20%'>Notes</th>
      <tr>
-    @foreach($registrations as $registration)
+    @foreach($registrations->sortBy('reatreatant.sort_name') as $registration)
 
     <tr>
         <td>{{$registration->retreatant->full_name}}</td>
@@ -23,6 +24,7 @@
         <td>{{$registration->retreatant->note_room_preference_text}}</td>
         <td>{{$registration->retreatant->note_health_text}}</td>
         <td>{{$registration->retreatant->note_dietary_text}}</td>
+        <td>{{$registration->notes}}</td>
 
     </tr>
     @endforeach
