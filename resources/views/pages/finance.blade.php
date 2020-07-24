a06f45cf24937b14988b22f1b3ee32bf39142507
Anthony Borrow, S.J <arborrow@jesuits.net>
2020-07-24 15:46:54 -0500

    changing AGC to FY21 on page.financial blade #418 (#419)

--- resources/views/pages/finance.blade.php
+++ resources/views/pages/finance.blade.php
@@ -3,7 +3,7 @@
 <h1>Welcome to the Finance Page</h1>
 @can('show-donation')
         {!! Html::link(action('DonationController@index'),'Donations',array('class' => 'btn btn-outline-dark'))!!}
-        {!! Html::link(action('DonationController@agc',2020),'AGC FY20',array('class' => 'btn btn-outline-dark'))!!}
+        {!! Html::link(action('DonationController@agc',2021),'AGC FY21',array('class' => 'btn btn-outline-dark'))!!}
         {!! Html::link(action('PageController@finance_reconcile_deposit_show'),'Unreconciled open deposits',array('class' => 'btn btn-outline-dark'))!!}
         {!! Html::link(action('DonationController@overpaid'),'Overpaid donations',array('class' => 'btn btn-outline-dark'))!!}
 
