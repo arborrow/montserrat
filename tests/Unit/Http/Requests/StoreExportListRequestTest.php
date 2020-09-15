52a923c5e695e6faa436112d46fc52ff0cb096bc
Anthony Borrow <arborrow@jesuits.net>
2020-09-15 07:39:04 -0500

    adding export list request tests

---
+++ tests/Unit/Http/Requests/StoreExportListRequestTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Tests\Unit\Http\Requests;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+/**
+ * @see \App\Http\Requests\StoreUomRequest
+ */
+class StoreExportListRequestTest extends TestCase
+{
+    /** @var \App\Http\Requests\StoreExportListRequest */
+    private $subject;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->subject = new \App\Http\Requests\StoreExportListRequest();
+    }
+
+    /**
+     * @test
+     */
+    public function authorize()
+    {
+        $actual = $this->subject->authorize();
+
+        $this->assertTrue($actual);
+    }
+
+    /**
+     * @test
+     */
+    public function rules()
+    {
+        $actual = $this->subject->rules();
+
+        $this->assertEquals([
+            'title' => 'string|max:125|required',
+            'label' => 'string|max:125|required',
+            'type' => 'in:'.implode(',', config('polanco.export_list_types')).'|required',
+            'fields' => 'string|nullable',
+            'filters' => 'string|nullable',
+            'start_date' => 'date|nullable',
+            'end_date' => 'date|nullable',
+            'next_scheduled_date' => 'date|nullable',
+            'last_run_date' => 'date|nullable',
+        ], $actual);
+    }
+
+    /**
+     * @test
+     */
+    public function messages()
+    {
+        $actual = $this->subject->messages();
+
+        $this->assertEquals([], $actual);
+    }
+
+    // test cases...
+}
