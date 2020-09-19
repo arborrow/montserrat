0d6cda717534e56da836b4f4ab5985552c312014
Anthony Borrow <arborrow@jesuits.net>
2020-09-19 05:35:43 -0500

    adding export list agc migration and model

---
+++ app/ExportListAgc.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use OwenIt\Auditing\Contracts\Auditable;
+
+class ExportListAgc extends Model implements Auditable
+{
+    use SoftDeletes;
+    use \OwenIt\Auditing\Auditable;
+
+    protected $table = 'export_list_agc';
+
+}
