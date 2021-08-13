a0f5711890b33aae0eef622628c27c9a9b6d8e5a
Anthony Borrow <arborrow@jesuits.net>
2021-08-13 18:00:55 -0400

    updating spanish version of registration canceled change email blade

--- resources/views/emails/es_ES/registration-canceled-change.blade.php
+++ resources/views/emails/es_ES/registration-canceled-change.blade.php
@@ -6,8 +6,8 @@
 </head>
 <body>
 	<p>{{config('polanco.site_name')}} would like to notify you that <a href="{{URL('registration',$registration->id)}}">Registration #{{ $registration->id }}</a> for {!!$registration->retreatant->contact_link_full_name!!} has recently been updated as follows:</p>
-	<p>from - <a href="{{ URL('retreat',$original_event->id) }}">{{ $original_event->retreat_name }}</a> ({{ $original_event->idnumber }}) - {{$original_event->retreat_type}}</p>
-	<p>to - <a href="{{ URL('retreat',$retreat->id) }}">{{ $retreat->retreat_name }}</a> ({{ $retreat->idnumber }}) - {{$retreat->retreat_type}}</p>
+	<p>for: <a href="{{ URL('retreat',$retreat->id) }}">{{ $retreat->retreat_name }}</a> ({{ $retreat->idnumber }}) - {{$retreat->retreat_type}}</p>
+	<p>The registration has been canceled; however, it appears to have a deposit in the amount of: ${{ $registration->deposit }}.
 	<p>Kindly ensure that the appropriate changes are made to the financial history.
 </body>
 </html>
