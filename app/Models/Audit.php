41ddf80d3312983ff52c5ef53d3d9732aec1d516
Anthony Borrow <arborrow@jesuits.net>
2021-08-15 13:23:59 -0400

    create audit factory and use in model, updated tests to create new audit record rather than first - fixes #449

--- app/Models/Audit.php
+++ app/Models/Audit.php
@@ -3,9 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 
 class Audit extends Model
 {
+    use HasFactory;
+
     protected $table = 'audits';
     protected $casts = [
         'old_values' => 'array',
---
+++ database/factories/AuditFactory.php
@@ -0,0 +1,46 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class AuditFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = \App\Models\Audit::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'user_type' => 'App\Models\User',
+            'user_id' => function () {
+                return \App\Models\User::factory()->create()->id;
+            },
+            'event' => $this->faker->randomElement(['updated', 'created', 'deleted']),
+            'auditable_type' => 'App\Contact',
+            'auditable_id' => function () {
+                return \App\Models\Contact::factory()->create()->id;
+            },
+            'old_values' => NULL,
+            'new_values' => NULL,
+            'url' => $this->faker->url(),
+            'ip_address' => $this->faker->ipv4(),
+            'user_agent' => $this->faker->userAgent(),
+            'tags' => NULL,
+            'created_at' => $this->faker->dateTimeThisMonth(),
+            'updated_at' => $this->faker->dateTimeThisMonth(),
+        ];
+    }
+}
