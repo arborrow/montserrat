992e253cf054665219340e2fa89656e64b3828a1
Anthony Borrow <arborrow@jesuits.net>
2021-10-15 07:39:19 -0400

    update store request tests to make phone validation optional if no twilio settings are available

--- tests/Unit/Http/Requests/StoreOrganizationRequestTest.php
+++ tests/Unit/Http/Requests/StoreOrganizationRequestTest.php
@@ -38,19 +38,38 @@ class StoreOrganizationRequestTest extends TestCase
     {
         $actual = $this->subject->rules();
 
-        $this->assertEquals([
-          'organization_name' => 'required',
-          'subcontact_type' => 'integer|min:0',
-          'email_main' => 'email|nullable',
-          'url_main' => 'url|nullable',
-          'url_facebook' => 'url|regex:/facebook\\.com\\/.+/i|nullable',
-          'url_google' => 'url|regex:/plus\\.google\\.com\\/.+/i|nullable',
-          'url_twitter' => 'url|regex:/twitter\\.com\\/.+/i|nullable',
-          'url_instagram' => 'url|regex:/instagram\\.com\\/.+/i|nullable',
-          'url_linkedin' => 'url|regex:/linkedin\\.com\\/.+/i|nullable',
-          'phone_main_phone' => 'phone|nullable',
-          'phone_main_fax' => 'phone|nullable',
-        ], $actual);
+        // if Twilio is enabled then validate phone numbers otherwise allow strings
+         if (null !== config('settings.twilio_sid') && null !== config('settings.twilio_token')) {
+
+            $this->assertEquals([
+              'organization_name' => 'required',
+              'subcontact_type' => 'integer|min:0',
+              'email_main' => 'email|nullable',
+              'url_main' => 'url|nullable',
+              'url_facebook' => 'url|regex:/facebook\\.com\\/.+/i|nullable',
+              'url_google' => 'url|regex:/plus\\.google\\.com\\/.+/i|nullable',
+              'url_twitter' => 'url|regex:/twitter\\.com\\/.+/i|nullable',
+              'url_instagram' => 'url|regex:/instagram\\.com\\/.+/i|nullable',
+              'url_linkedin' => 'url|regex:/linkedin\\.com\\/.+/i|nullable',
+              'phone_main_phone' => 'phone|nullable',
+              'phone_main_fax' => 'phone|nullable',
+            ], $actual);
+        } else {
+            $this->assertEquals([
+              'organization_name' => 'required',
+              'subcontact_type' => 'integer|min:0',
+              'email_main' => 'email|nullable',
+              'url_main' => 'url|nullable',
+              'url_facebook' => 'url|regex:/facebook\\.com\\/.+/i|nullable',
+              'url_google' => 'url|regex:/plus\\.google\\.com\\/.+/i|nullable',
+              'url_twitter' => 'url|regex:/twitter\\.com\\/.+/i|nullable',
+              'url_instagram' => 'url|regex:/instagram\\.com\\/.+/i|nullable',
+              'url_linkedin' => 'url|regex:/linkedin\\.com\\/.+/i|nullable',
+              'phone_main_phone' => 'string|nullable',
+              'phone_main_fax' => 'string|nullable',
+            ], $actual);
+
+        }
     }
 
     /**
--- tests/Unit/Http/Requests/StoreParishRequestTest.php
+++ tests/Unit/Http/Requests/StoreParishRequestTest.php
@@ -38,21 +38,41 @@ class StoreParishRequestTest extends TestCase
     {
         $actual = $this->subject->rules();
 
-        $this->assertEquals([
-          'organization_name' => 'required',
-          'diocese_id' => 'integer|min:0',
-          'pastor_id' => 'integer|min:0',
-          'parish_email_main' => 'email|nullable',
-          'url_main' => 'url|nullable',
-          'url_facebook' => 'url|regex:/facebook\\.com\\/.+/i|nullable',
-          'url_google' => 'url|regex:/plus\\.google\\.com\\/.+/i|nullable',
-          'url_twitter' => 'url|regex:/twitter\\.com\\/.+/i|nullable',
-          'url_instagram' => 'url|regex:/instagram\\.com\\/.+/i|nullable',
-          'url_linkedin' => 'url|regex:/linkedin\\.com\\/.+/i|nullable',
-          'phone_main_phone' => 'phone|nullable',
-          'phone_main_fax' => 'phone|nullable',
-          'parish_note' => 'string|nullable',
-        ], $actual);
+        // if Twilio is enabled then validate phone numbers otherwise allow strings
+         if (null !== config('settings.twilio_sid') && null !== config('settings.twilio_token')) {
+
+            $this->assertEquals([
+              'organization_name' => 'required',
+              'diocese_id' => 'integer|min:0',
+              'pastor_id' => 'integer|min:0',
+              'parish_email_main' => 'email|nullable',
+              'url_main' => 'url|nullable',
+              'url_facebook' => 'url|regex:/facebook\\.com\\/.+/i|nullable',
+              'url_google' => 'url|regex:/plus\\.google\\.com\\/.+/i|nullable',
+              'url_twitter' => 'url|regex:/twitter\\.com\\/.+/i|nullable',
+              'url_instagram' => 'url|regex:/instagram\\.com\\/.+/i|nullable',
+              'url_linkedin' => 'url|regex:/linkedin\\.com\\/.+/i|nullable',
+              'phone_main_phone' => 'phone|nullable',
+              'phone_main_fax' => 'phone|nullable',
+              'parish_note' => 'string|nullable',
+            ], $actual);
+        } else {
+            $this->assertEquals([
+              'organization_name' => 'required',
+              'diocese_id' => 'integer|min:0',
+              'pastor_id' => 'integer|min:0',
+              'parish_email_main' => 'email|nullable',
+              'url_main' => 'url|nullable',
+              'url_facebook' => 'url|regex:/facebook\\.com\\/.+/i|nullable',
+              'url_google' => 'url|regex:/plus\\.google\\.com\\/.+/i|nullable',
+              'url_twitter' => 'url|regex:/twitter\\.com\\/.+/i|nullable',
+              'url_instagram' => 'url|regex:/instagram\\.com\\/.+/i|nullable',
+              'url_linkedin' => 'url|regex:/linkedin\\.com\\/.+/i|nullable',
+              'phone_main_phone' => 'string|nullable',
+              'phone_main_fax' => 'string|nullable',
+              'parish_note' => 'string|nullable',
+            ], $actual);
+        }
     }
 
     /**
