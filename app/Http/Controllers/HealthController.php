4c03fdb515022d5c737095423de925569e16b068
Anthony Borrow <arborrow@jesuits.net>
2021-10-15 10:44:17 -0400

    add abandoned_payment database health check

--- app/Http/Controllers/HealthController.php
+++ app/Http/Controllers/HealthController.php
@@ -25,7 +25,8 @@ class HealthController extends Controller
         $results->put('primary_address',$this->check_primary_address());
         $results->put('primary_email',$this->check_primary_email());
         $results->put('primary_phone',$this->check_primary_phone());
-        //dd($results['primary_email']);
+        $results->put('abandoned_payments',$this->check_abandoned_payments());
+
         return view('health.index', compact('results'));   //
     }
 
@@ -69,6 +70,24 @@ class HealthController extends Controller
         $phone_primary = DB::table('phone')->whereIsPrimary(1)->groupBy('contact_id')->havingRaw('count(id) > 1')->select('contact_id','phone')->get();
 
         return $phone_primary;   //
+    }
+
+    /**
+     * Run the abandoned payments check to ensure there are no payments with a deleted donation
+     *
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
+    public function check_abandoned_payments()
+    {
+        $this->authorize('show-admin');
+        $results = collect([]);
+
+        $abandoned_payments = DB::table('Donations_payment')->where('Donations_payment.payment_amount','>',0)
+        ->leftJoin('Donations','Donations.donation_id','=','Donations_payment.donation_id')
+        ->whereNotNull('Donations.deleted_at')
+        ->select('Donations.contact_id','Donations_payment.donation_id','Donations.donation_amount','Donations_payment.payment_id','Donations_payment.payment_amount')
+        ->get();
+        return $abandoned_payments;   //
     }
 
 
