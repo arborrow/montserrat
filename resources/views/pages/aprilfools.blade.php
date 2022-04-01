832c97412bf29a42eefa377d4a089c02978133b6
Anthony Borrow <arborrow@jesuits.net>
2022-04-01 07:45:55 -0400

    back hack link

--- resources/views/pages/aprilfools.blade.php
+++ resources/views/pages/aprilfools.blade.php
@@ -3,7 +3,7 @@
 <h1>Why is the FBI watching me?</h1>
 <img src="https://avatarfiles.alphacoders.com/240/240483.jpg" alt="FBI" title="FBI is watching"><hr />
 <p>It may be related to the fact that Fr. Anthony is a fan of <a href="https://www.imdb.com/title/tt0452046/">Criminal Minds</a> and has a crush on <a href="https://criminalminds.fandom.com/wiki/Penelope_Garcia">Penelope Garcia</a>.
-  Is Penelope flirting back and back hacked Polanco from Quantico?
+  Is Penelope flirting back and <a href="https://www.youtube.com/watch?v=WqtbN_VHXVA">back hacked</a> Polanco from Quantico?
   Hmm, that seems unlikely. The more likely reason the FBI is watching you today is because it is April Fool's Day and Polanco decided to have a little fun ;) </p>
 <p>Other reasons why the FBI might be watching is because you are doing bad things :p
   <ol>
