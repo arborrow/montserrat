c7dfea758c0aa7fbf8f34b916e05c833fd2605a2
Anthony Borrow <arborrow@jesuits.net>
2022-06-13 00:14:11 -0400

    correct class name for mailgun permissions seeder

--- database/seeders/MailgunPermissionSeeder.php
+++ database/seeders/MailgunPermissionSeeder.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class MailgunPermissionSeeder extends Seeder
+class MailgunPermissionsSeeder extends Seeder
 {
     /**
      * Run the database seeds.
