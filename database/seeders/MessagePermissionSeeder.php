c26e0ea8e90e22523801ba7a2ce669b84b6e26c2
Anthony Borrow <arborrow@jesuits.net>
2022-06-13 00:17:19 -0400

    rename mailgun seeder to message seeder

--- database/seeders/MessagePermissionSeeder.php
+++ database/seeders/MessagePermissionSeeder.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class MailgunPermissionsSeeder extends Seeder
+class MessagePermissionsSeeder extends Seeder
 {
     /**
      * Run the database seeds.
