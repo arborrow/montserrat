cf049464c0cf624f0228d65cc4a63bd3f0c50a76
Anthony Borrow <arborrow@jesuits.net>
2022-12-09 15:07:03 +0100

    uncomment updating of optional donation_id field

--- app/Http/Controllers/GiftCertificateController.php
+++ app/Http/Controllers/GiftCertificateController.php
@@ -89,7 +89,7 @@ class GiftCertificateController extends Controller
         $gift_certificate->purchaser_id = ($request->input('purchaser_id') > 0 ) ? $request->input('purchaser_id') : null;
         $gift_certificate->recipient_id = ($request->input('recipient_id') > 0 ) ? $request->input('recipient_id') : null;
         $gift_certificate->participant_id = $request->input('participant_id');
-        // $gift_certificate->donation_id = $request->input('donation_id');
+        $gift_certificate->donation_id = $request->input('donation_id');
         $gift_certificate->sequential_number = $request->input('sequential_number');
         $gift_certificate->squarespace_order_number = $request->input('squarespace_order_number');
         $gift_certificate->purchase_date = $request->input('purchase_date');
@@ -182,7 +182,7 @@ class GiftCertificateController extends Controller
         $gift_certificate->purchaser_id = $request->input('purchaser_id');
         $gift_certificate->recipient_id = $request->input('recipient_id');
         $gift_certificate->participant_id = $request->input('participant_id');
-        // $gift_certificate->donation_id = $request->input('donation_id');
+        $gift_certificate->donation_id = $request->input('donation_id');
         $gift_certificate->sequential_number = $request->input('sequential_number');
         $gift_certificate->squarespace_order_number = $request->input('squarespace_order_number');
         $gift_certificate->purchase_date = $request->input('purchase_date');
